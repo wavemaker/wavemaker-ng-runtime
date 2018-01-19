ddfe60cab0943d399fa201897a76e30d8f7c6096
abhilashsandi <abhilash.sandi@wavemaker.com>
2018-01-19 12:41:28 +0530

    Run change detection cycle
    Change-Id: I1aec0cb08d5f42d2006259aeb7abba35a4ca5deb

--- runtime/app.component.ts
+++ runtime/app.component.ts
@@ -1,18 +1,19 @@
-import { Component, DoCheck } from '@angular/core';
+import { Component, DoCheck, ApplicationRef } from '@angular/core';
 import { PipeProvider } from './services/pipe-provider.service';
 import { setPipeProvider } from '@utils/expression-parser';
-import { $digest } from '@utils/watcher';
+import { $invokeWatchers, setAppRef } from '@utils/watcher';
 
 @Component({
     selector: 'div#wm-app-content',
     template: '<router-outlet></router-outlet>'
 })
 export class AppComponent implements DoCheck {
-    constructor(_pipeProvider: PipeProvider) {
+    constructor(_pipeProvider: PipeProvider, _appRef: ApplicationRef) {
         setPipeProvider(_pipeProvider);
+        setAppRef(_appRef);
     }
 
     ngDoCheck() {
-        $digest();
+        $invokeWatchers();
     }
 }
