aea5afa0ee3abe4fb15dafa4bab6e14a75ad2737
vinay <vinay.kaparthi@wavemaker.com>
2018-01-24 18:19:04 +0530

    Code Refactor
    Change-Id: If5ea608fe7e515784631b3256764302d4591fc01

--- components/components.module.ts
+++ components/components.module.ts
@@ -1,34 +1,35 @@
-import { AnchorDirective } from './widgets/anchor/anchor.directive';
-import { ButtonDirective } from './widgets/button/button.directive';
-import { LabelDirective } from './widgets/label/label.directive';
-import { DateComponent } from './widgets/date/date.component';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { RadiosetComponent } from './widgets/radioset/radioset.component';
-import { TextDirective } from './widgets/text/text.directive';
-import { TextareaDirective } from './widgets/textarea/textarea.directive';
-import { LayoutgridDirective } from './widgets/layoutgrid/layoutgrid.directive';
-import { GridrowDirective } from './widgets/layoutgrid/gridrow/gridrow.directive';
-import { GridcolumnDirective } from './widgets/layoutgrid/gridcolumn/gridcolumn.directive';
+
+import { AnchorDirective } from './widgets/anchor/anchor.directive';
 import { BsDatepickerModule } from 'ngx-bootstrap';
+import { ButtonDirective } from './widgets/button/button.directive';
+import { DateComponent } from './widgets/date/date.component';
+import { GridcolumnDirective } from './widgets/layoutgrid/gridcolumn/gridcolumn.directive';
+import { GridrowDirective } from './widgets/layoutgrid/gridrow/gridrow.directive';
+import { LabelDirective } from './widgets/label/label.directive';
+import { LayoutgridDirective } from './widgets/layoutgrid/layoutgrid.directive';
 import { PanelComponent } from './widgets/panel/panel.component';
 import { PanelFooterDirective } from './widgets/panel/panel-footer/panel-footer.component';
+import { RadiosetComponent } from './widgets/radioset/radioset.component';
+import { TextareaDirective } from './widgets/textarea/textarea.directive';
+import { TextDirective } from './widgets/text/text.directive';
 
 
 const wmComponents = [
     AnchorDirective,
     ButtonDirective,
-    LabelDirective,
-    TextDirective,
-    TextareaDirective,
-    RadiosetComponent,
-    LayoutgridDirective,
-    GridrowDirective,
-    GridcolumnDirective,
     DateComponent,
+    GridcolumnDirective,
+    GridrowDirective,
+    LabelDirective,
+    LayoutgridDirective,
     PanelComponent,
-    PanelFooterDirective
+    PanelFooterDirective,
+    RadiosetComponent,
+    TextareaDirective,
+    TextDirective
 ];
 
 @NgModule({
