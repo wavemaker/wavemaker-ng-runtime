063f24115bce5c1d68d1bfe2e89eda371e6fd6f4
Nikhilesh <nikhilesh.chakravarthy@wavemaker.com>
2018-02-21 16:06:24 +0530

    Search Component: Added Search Component
    Change-Id: Ie85ae7aa1e2757063d79595bfca4e041d50038f4

---
+++ components/widgets/search/search.component.ts
@@ -0,0 +1,329 @@
+import { Component, ElementRef, Injector, ChangeDetectorRef, Output, EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs/Rx';
+import { TypeaheadMatch } from 'ngx-bootstrap/typeahead';
+import { BaseComponent } from '../base/base.component';
+import { getEvaluatedData } from '../../utils/widget-utils';
+import { getClonedObject } from '../../../utils/utils';
+import { getOrderedDataSet } from '../../utils/form-utils';
+import { registerProps } from './search.props';
+import { $appDigest } from '@utils/watcher';
+import { styler } from '../../utils/styler';
+
+declare const _;
+
+const ALL_FIELDS: string = 'All Fields';
+
+const WIDGET_CONFIG = {widgetType: 'wm-search', hostClass: 'app-search input-group'};
+
+registerProps();
+/**
+ * The Search component
+ * Represents search widget with dataset, displaylabel, datafield etc., properties.
+ * Example of usage:
+ * <example-url>http://localhost:4200/search</example-url>
+ *
+ */
+@Component({
+  selector: '[wmSearch]',
+  templateUrl: './search.component.html'
+})
+export class SearchComponent extends BaseComponent {
+    /**
+     * The onChange event for the search input
+     */
+    @Output() change = new EventEmitter();
+    /**
+     * The onKeyDown event for the search input
+     */
+    @Output() keydown = new EventEmitter();
+    @Output() select = new EventEmitter();
+    @Output() submit = new EventEmitter();
+
+    displaylabel;
+    datafield;
+    casesensitive;
+    searchkey;
+    imagewidth;
+    orderby;
+    displayimagesrc;
+
+    /**
+     * Private property to map the datavalue
+     */
+    private proxyDatavalue;
+    private _datavalue;
+    private _dataset;
+    /**
+     * The dataset property to set the search results
+     */
+    set dataset(data) {
+        this._dataset = data;
+    }
+
+    get dataset() {
+        return this.itemsList;
+    }
+
+    get datavalue() {
+        return this.proxyDatavalue;
+    }
+    /**
+     * The default value to be assigned on the search input
+     */
+    set datavalue(newVal) {
+        this._datavalue = newVal;
+    }
+
+    onDatasetChange(newVal?) {
+        let dataSet;
+
+        newVal = newVal || this._dataset;
+
+        // get the variable-data w.r.t the variable type
+        newVal = (newVal && newVal.data) || newVal;
+        // set data-set
+        dataSet = getClonedObject(newVal);
+        // if data-set is an array, show the 'listOfObjects' mode
+        if (_.isArray(dataSet)) {
+            // Removing null values from dataSet.
+            _.remove(dataSet, (o) => {
+                return _.isUndefined(o) || _.isNull(o);
+            });
+
+            dataSet = getOrderedDataSet(dataSet, this.orderby);
+            // check if dataSet contains list of objects, then switch to 'listOfObjects', else display 'default'
+            if (_.isObject(dataSet[0])) {
+                _.forEach(dataSet, (eachItem, index) => {
+                    const itemValue = dataSet[index];
+                    if (_.isObject(itemValue)) {
+                        // convert display-label-value to string, as ui.typeahead expects only strings
+                        itemValue.wmDisplayLabel = getEvaluatedData(eachItem, {displayexpression: this.displaylabel});
+                        // to save all the image urls
+                        itemValue.wmImgSrc   = getEvaluatedData(eachItem, {displayexpression: this.displayimagesrc});
+                        itemValue.wmImgWidth = this.imagewidth;
+                    }
+                });
+            } else {
+                // convert all the values in the array to strings
+                _.forEach(dataSet, (val, index) => {
+                    dataSet[index] = _.toString(val);
+                });
+            }
+
+            // set the itemList. If page number is greater than 1, append the results.
+            this.itemsList = this.page > 1 ? this.itemsList.concat(dataSet) : dataSet;
+
+        } else if (_.isString(dataSet) && dataSet.trim()) {
+            // make the string an array, for ex. => if dataSet is 1,2,3 then make it [1,2,3]
+            this.dataset = _.split(dataSet, ',');
+            return;
+        } else if (_.isObject(dataSet)) {
+            this.dataset = _.join(Object.keys(dataSet), ',');
+            return;
+        }
+        this.formattedDataSet = this.page > 1 ? this.formattedDataSet.concat(dataSet) : dataSet;
+
+        $appDigest();
+        // update the queryModel, if the default value is given and formatted Dataset is defined.
+        /*
+        if (!isVariableUpdateRequired($is, element.scope(), true) || ($is.formattedDataSet.length && !$is.isDefaultValueExist && WM.isDefined($is.datavalue) && $is.datavalue !== '')) {
+            updateQueryModel($is, element);
+            $is.isDefaultValueExist = true;
+        }
+        */
+    }
+
+    onDataValueChange(newVal?) {
+        let model;
+        newVal = newVal || this._datavalue;
+        if (newVal) {
+            model = this.getDataObjbyDataField(newVal);
+            if (!_.isEmpty(model)) {
+                model = model[0];
+                this.selectedItem = getClonedObject(model);
+                this.proxyDatavalue = (this.datafield && this.datafield !== ALL_FIELDS) ? (this.selectedItem  && _.get(this.selectedItem, this.datafield)) : this.selectedItem;
+                this.queryModel = getEvaluatedData(model, {displayexpression: this.displaylabel});
+            } else {
+                this.selectedItem = this.proxyDatavalue = this.queryModel = undefined;
+            }
+        }
+        $appDigest();
+    }
+
+    get query() {
+        return this.queryModel;
+    }
+
+    set query(newVal) {
+        this.queryModel = newVal;
+        $appDigest();
+    }
+    /**
+     * The result of the search input when dropdown is open
+     */
+    result: any;
+    /**
+     * The current page of the result set
+     */
+    private page = 0;
+    /**
+     * Private property to map the formatted dataset based on the collection
+     */
+    private formattedDataSet;
+    /**
+     * Private property to map the dataSource observable to the typeahead
+     */
+    dataSource: Observable<any>;
+    /**
+     * Private property to map the queryModel value
+     */
+    queryModel: string;
+    /**
+     * Private property to get the complete model object of the selected item
+     */
+    private selectedItem;
+    /**
+     * Private property to map the itemsList prepared from the dataset
+     */
+    private itemsList: any;
+    /**
+     * Private property to flag the loading status of the items
+     */
+    private _loadingItems;
+
+    constructor(inj: Injector, elRef: ElementRef, private cdr: ChangeDetectorRef) {
+        super(WIDGET_CONFIG, inj, elRef, cdr);
+        styler(this.$element, this);
+    }
+    /**
+     * Private method to get the the dataobj by the datafield
+     */
+    private getDataObjbyDataField(matchValue) {
+        return _.filter(this.dataset, (item) => {
+           if (item[this.datafield] === matchValue) {
+               return true;
+           }
+        });
+    }
+    /**
+     * Private method to get the unique objects by the data field
+     */
+    private getUniqObjsByDataField(data, dataField, displayField, isLocalSearch) {
+        let uniqData;
+        const isAllFields = dataField === ALL_FIELDS;
+
+        uniqData = isAllFields ? _.uniqWith(data, _.isEqual) : _.uniqBy(data, dataField);
+
+        if (!displayField && isLocalSearch) {
+            return uniqData;
+        }
+
+        // return objects having non empty datafield and display field values.
+        return _.filter(uniqData, (obj) => {
+            if (isAllFields) {
+                return _.trim(obj.wmDisplayLabel);
+            }
+            return !!_.trim(_.get(obj, dataField)) && _.trim(obj.wmDisplayLabel);
+        });
+    }
+    /**
+     * Private method to filter the search fields based on the search inputs, returns ScalableObservable instance with the data
+     */
+    private filterData(token: string): Observable<any> {
+        const keys = _.split(this.searchkey, ',');
+        /*push the wmDisplayLabel to match the display label formatted*/
+        keys.push('wmDisplayLabel');
+        const result = _.filter(this.dataset, (item: any) => {
+            return keys.some((key) => {
+                let a = _.get(item, key),
+                    b = token;
+                if (!this.casesensitive) {
+                    a = _.toLower(_.toString(a));
+                    b = _.toLower(_.toString(b));
+                }
+                return _.includes(a, b);
+            });
+        });
+
+        this.updateResult(result);
+        return Observable.of(this.getUniqObjsByDataField(result, this.datafield, this.displaylabel, true));
+    }
+    /**
+     * Private method to update the result and change the datavalue if results are empty
+     */
+    private updateResult(matchedItems) {
+        // on typing the value in input, make the datavalue undefined, if no matches found.
+        if (!matchedItems || _.isEmpty(matchedItems)) {
+            this.proxyDatavalue = undefined;
+        }
+        this.result = (this.datafield === ALL_FIELDS || !this.datafield) ? matchedItems : _.map(matchedItems, this.datafield);
+        $appDigest();
+    }
+    /**
+     * Private method wrapper to map the selectedValue to onSelect, onSubmit Events
+     */
+    private onTypeAheadSelect($event: TypeaheadMatch | any) {
+        $event = $event || <TypeaheadMatch>{};
+
+        let $item = getClonedObject($event.item);
+
+        $item = this.selectedItem = $item || (this.proxyDatavalue === _.get(this.selectedItem, this.datafield) ? this.selectedItem : undefined);
+
+        const $label = $item && getEvaluatedData($item, {displayexpression: this.displaylabel});
+
+        if ($item && ($item.wmImgSrc || $item.wmDisplayLabel)) {
+            delete $item.wmImgSrc;
+            delete $item.wmImgWidth;
+            delete $item.wmDisplayLabel;
+        }
+
+        // add the selected object to the event.data and send to the user
+        $event.data = {'item': $item, 'model': $item, 'label': $label, 'query': $label};
+
+        delete $event.item;
+        // set selected item on widget's exposed property
+        this.proxyDatavalue = (this.datafield && this.datafield !== ALL_FIELDS) ? ($item  && _.get($item, this.datafield)) : $item;
+        this.queryModel = $label;
+        this.result     = [];
+        // call user 'onSubmit & onSelect' fn
+        this.select.emit({$event: $event, $scope: this, newVal: this.proxyDatavalue});
+        this.submit.emit({$event: $event, $scope: this});
+    }
+    /**
+     * Private method to change the loading status of the flag
+     */
+    private setLoadingItemsFlag(flag) {
+        this._loadingItems = flag;
+    }
+    /**
+     * Private method wrappper to the keyDown event
+     */
+    private executeKeyDownEvent = ($event) => {
+        this.keydown.emit($event);
+    }
+
+    _ngOnInit() {
+        this.dataSource = Observable.create((observer: any) => {
+            observer.next(this.queryModel);
+        }).mergeMap((token: string) => this.filterData(token));
+    }
+
+    onPropertyChange(key, newVal, oldVal) {
+        switch (key) {
+            case 'scopedataset':
+            case 'dataset':
+                this.onDatasetChange(newVal);
+                break;
+            case 'scopedatavalue':
+            case 'datavalue':
+                this.onDataValueChange(newVal);
+                break;
+            case 'displaylabel':
+            case 'displayimagesrc':
+            case 'datafield':
+                this.onDatasetChange();
+                this.onDataValueChange();
+                break;
+        }
+    }
+}
