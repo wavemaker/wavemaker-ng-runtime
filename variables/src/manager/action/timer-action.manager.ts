6ba7675cc1cb2ce1221eaa817382493734d82a01
ShubhamAgarwal <shubham.agarwal@wavemaker.com>
2018-04-16 16:01:57 +0530

    Timer Action: Implementation
    Change-Id: I4af85dc851b9a4f2054ef26cf6b69121b43563b8

--- variables/src/manager/action/timer-action.manager.ts
+++ variables/src/manager/action/timer-action.manager.ts
@@ -1,7 +1,32 @@
+import { isDefined } from '@wm/core';
+
 import { BaseActionManager } from './base-action.manager';
+import { initiateCallback } from '../../../src/util/variable/variables.utils';
+import { CONSTANTS } from '../../constants/variables.constants';
 
 export class TimerActionManager extends BaseActionManager {
     trigger(variable, options, success, error) {
-        console.log('triggering timer in some "time", trust me!!');
+        if (isDefined(variable._promise) || CONSTANTS.isStudioMode) {
+            return;
+        }
+        const repeatTimer = variable.repeating,
+            delay = variable.delay || CONSTANTS.DEFAULT_TIMER_DELAY,
+            event = 'onTimerFire',
+            // callBackScope = {} || options && options.scope,
+            exec = function () {
+                initiateCallback(event, variable, {});
+            };
+
+        variable._promise = repeatTimer ? setInterval(exec, delay) : setTimeout(function () {
+            exec();
+            variable._promise = undefined;
+        }, delay);
+
+        /*// destroy the timer on scope destruction
+        callBackScope.$on('$destroy', function () {
+            variable.cancel(variable._promise);
+        });*/
+
+        return variable._promise;
     }
 }
