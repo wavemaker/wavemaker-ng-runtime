bfe76aac907526432cc4a221aaa1d715f2c03f3d
lovin ahmed <lovin.ahmed@wavemaker.com>
2018-05-04 15:46:28 +0530

    WM0# Aria changes for button and button group.
    Change-Id: Ice933a36a3d6281aed9d671642d6c15f69ecb6bf

--- components/src/widgets/common/anchor/anchor.build.ts
+++ components/src/widgets/common/anchor/anchor.build.ts
@@ -4,7 +4,7 @@ const tagName = 'a';
 
 register('wm-anchor', (): IBuildTaskDef => {
     return {
-        pre: attrs => `<${tagName} wmAnchor role="button" data-identifier="anchor" ${getAttrMarkup(attrs)}>`,
+        pre: attrs => `<${tagName} wmAnchor role="link" data-identifier="anchor" ${getAttrMarkup(attrs)}>`,
         post: () => `</${tagName}>`
     };
 });
