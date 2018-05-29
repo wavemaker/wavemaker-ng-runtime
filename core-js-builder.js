a5db0c5618801c9fd6853f792f208b62fdb64248
vinay <vinay.kaparthi@wavemaker.com>
2018-05-29 12:03:33 +0530

    Fix for issue: Build failure issue on systems with nodejs <= 6
    Change-Id: Iefb985f48616b9cbfe01b8f74063b0464139f4d7

--- core-js-builder.js
+++ core-js-builder.js
@@ -5,10 +5,8 @@ require('core-js-builder')({
     //blacklist: ['es6.reflect'],    // blacklist of modules / namespaces, by default - empty list
     library: false,                // flag for build without global namespace pollution, by default - false
     umd: true                      // use UMD wrapper for export `core` object, by default - true
-}).then(code => {
-    fs.writeFileSync('./dist/tmp/core-js.umd.js', code, () => {
-        console.log(error);
-    });
-}).catch(error => {
+}).then(function(code) {
+    fs.writeFileSync('./dist/tmp/core-js.umd.js', code);
+}).catch(function (error) {
     console.log(error);
 });
