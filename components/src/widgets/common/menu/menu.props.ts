1c4b923627b9b57ab9d930e0a466b22894cc31d4
PunithMithra <punith.mitra@wavemaker.com>
2018-05-30 19:05:08 +0530

    Refactoring Menu Component
    Change-Id: Ibb7c734c834591b8e1372a58475ff5c5c7a86d43

--- components/src/widgets/common/menu/menu.props.ts
+++ components/src/widgets/common/menu/menu.props.ts
@@ -1,29 +1,34 @@
 import { PROP_NUMBER, PROP_STRING, PROP_STRING_NOTIFY, register } from '../../framework/widget-props';
+import { PROP_BOOLEAN } from '@wm/components';
 
 export const registerProps = () => {
     register(
         'wm-menu',
         new Map(
             [
-                ['accessroles', {value: 'Everyone', ...PROP_STRING_NOTIFY}],
+                ['accessroles', {value: 'Everyone', ...PROP_STRING}],
                 ['animateitems', PROP_STRING],
                 ['autoclose', {value: 'always', ...PROP_STRING}],
                 ['caption', PROP_STRING],
                 ['dataset', {value: 'Menu Item 1, Menu Item 2, Menu Item 3', ...PROP_STRING_NOTIFY}],
+                ['hint', {value: '', ...PROP_STRING}],
                 ['iconclass', PROP_STRING],
                 ['itemaction', PROP_STRING_NOTIFY],
                 ['itemchildren', PROP_STRING_NOTIFY],
                 ['itemicon', PROP_STRING_NOTIFY],
                 ['itemlabel', PROP_STRING_NOTIFY],
                 ['itemlink', PROP_STRING_NOTIFY],
-                ['linktarget', PROP_STRING_NOTIFY],
+                ['linktarget', {value: '', ...PROP_STRING}],
                 ['menuclass', PROP_STRING],
                 ['menulayout', PROP_STRING],
-                ['menuposition', PROP_STRING_NOTIFY],
+                ['menuposition', PROP_STRING],
+                ['name', PROP_STRING],
                 ['orderby', PROP_STRING],
                 ['shortcutkey', PROP_STRING],
+                ['show', {value: true, ...PROP_BOOLEAN}],
                 ['tabindex', {value: 0, ...PROP_NUMBER}],
-                ['type', {value: 'menu', ...PROP_STRING}]
+                ['type', {value: 'menu', ...PROP_STRING}],
+                ['userrole', PROP_STRING]
             ]
         )
     );
