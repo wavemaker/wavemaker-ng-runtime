c5409d01a806867dd5d553fc291e226400c45f2b
vinay <vinay.kaparthi@wavemaker.com>
2018-06-26 10:36:52 +0530

    Fix for issue: app-chart class is not applied for charts
    Change-Id: I25331d54a28db5131ba99702684dc93ed3ac3411

--- components/src/widgets/common/chart/chart.build.ts
+++ components/src/widgets/common/chart/chart.build.ts
@@ -4,7 +4,7 @@ const tagName = 'div';
 
 register('wm-chart', (): IBuildTaskDef => {
     return {
-        pre: attrs => `<${tagName} wmChart class="app-chart" redrawable ${getAttrMarkup(attrs)}>`,
+        pre: attrs => `<${tagName} wmChart redrawable ${getAttrMarkup(attrs)}>`,
         post: () => `</${tagName}>`
     };
 });
