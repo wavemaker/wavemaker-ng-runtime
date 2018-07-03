a29adee093d6c41578eeea04b05060de98d405d7
Punith Mitra <punith.mitra@wavemaker.com>
2018-07-04 00:33:21 +0530

    Implemented keyboard navigation for Menu Component.
    Change-Id: I2f2c77fffd672ed274294557d8e82d4b89874ba6

--- components/src/widgets/common/menu/menu.component.ts
+++ components/src/widgets/common/menu/menu.component.ts
@@ -11,6 +11,24 @@ import { DatasetAwareNavComponent } from '../base/dataset-aware-nav.component';
 
 declare const _;
 
+export const KEYBOARD_MOVEMENTS = {
+    MOVE_UP: 'UP-ARROW',
+    MOVE_LEFT : 'LEFT-ARROW',
+    MOVE_RIGHT : 'RIGHT-ARROW',
+    MOVE_DOWN : 'DOWN-ARROW',
+    ON_ENTER : 'ENTER',
+    ON_TAB : 'TAB',
+    ON_ESCAPE : 'ESC'
+};
+
+export const MENU_POSITION = {
+    UP_LEFT: 'up,left',
+    UP_RIGHT: 'up,right',
+    DOWN_LEFT: 'down,left',
+    DOWN_RIGHT: 'down,right',
+    INLINE: 'inline'
+};
+
 registerProps();
 
 const POSITION = {
@@ -49,15 +67,55 @@ export class MenuComponent extends DatasetAwareNavComponent implements OnInit, O
     public autoclose: string;
 
     private menuCaret: string = 'fa-caret-down';
+    private _selectFirstItem = false;
 
-    @HostListener('onShown')
-    @HostListener('onHidden') triggerDigest () {
+    @HostListener('onShown') onShow() {
+        if (this._selectFirstItem) {
+            setTimeout(() => {
+                this.$element.find('> ul[wmmenudropdown] li.app-menu-item:first > a').focus();
+            });
+        }
         $appDigest();
+    }
+    @HostListener('onHidden') onHide () {
+        this.$element.find('>.dropdown-toggle').focus();
+        this.$element.find('li').removeClass('open');
+        this._selectFirstItem = false;
+        $appDigest();
+    }
+
+    @HostListener('keydown.arrowup', ['$event', '"UP-ARROW"'])
+    @HostListener('keydown.arrowdown', ['$event', '"DOWN-ARROW"'])
+    @HostListener('keydown.arrowright', ['$event', '"RIGHT-ARROW"'])
+    @HostListener('keydown.arrowleft', ['$event', '"LEFT-ARROW"'])
+    @HostListener('keydown.enter', ['$event', '"ENTER"']) onKeyDown($event, eventAction) {
+        const KEY_MOVEMENTS = _.clone(KEYBOARD_MOVEMENTS);
+        if (this.menuposition === MENU_POSITION.UP_RIGHT) {
+            KEY_MOVEMENTS.MOVE_UP = 'DOWN-ARROW';
+            KEY_MOVEMENTS.MOVE_DOWN = 'UP-ARROW';
+        } else if (this.menuposition === MENU_POSITION.UP_LEFT) {
+            KEY_MOVEMENTS.MOVE_UP = 'DOWN-ARROW';
+            KEY_MOVEMENTS.MOVE_DOWN = 'UP-ARROW';
+            KEY_MOVEMENTS.MOVE_LEFT = 'RIGHT-ARROW';
+            KEY_MOVEMENTS.MOVE_RIGHT = 'LEFT-ARROW';
+        } else if (this.menuposition === MENU_POSITION.DOWN_LEFT) {
+            KEY_MOVEMENTS.MOVE_LEFT = 'RIGHT-ARROW';
+            KEY_MOVEMENTS.MOVE_RIGHT = 'LEFT-ARROW';
+        }
+
+        if (eventAction === KEY_MOVEMENTS.MOVE_DOWN || eventAction === KEY_MOVEMENTS.MOVE_RIGHT || eventAction === KEY_MOVEMENTS.ON_ENTER) {
+            this._selectFirstItem = true;
+            this.bsDropdown.show();
+            $event.preventDefault();
+        } else if (eventAction === KEY_MOVEMENTS.MOVE_UP || eventAction === KEY_MOVEMENTS.MOVE_LEFT) {
+            this.bsDropdown.hide();
+            $event.preventDefault();
+        }
     }
 
     constructor(
         inj: Injector,
-        @Self() @Optional() private dropdown: BsDropdownDirective
+        @Self() @Optional() public bsDropdown: BsDropdownDirective
     ) {
         super(inj, WIDGET_CONFIG);
     }
@@ -74,7 +132,7 @@ export class MenuComponent extends DatasetAwareNavComponent implements OnInit, O
         }
 
         if (key === 'autoclose') {
-            this.dropdown.autoClose = nv !== 'disabled';
+            this.bsDropdown.autoClose = nv !== 'disabled';
         } else {
             super.onPropertyChange(key, nv, ov);
         }
