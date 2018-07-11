0492320d2f55c58cc9f490aecdebf9955022e5c1
vinay <vinay.kaparthi@wavemaker.com>
2018-07-11 15:51:56 +0530

    Add $watch to angular deprecations
    Change-Id: Idf9422234c870a483979efae74dae1f0213f39b2

--- runtime/services/angular1.polyfills.ts
+++ runtime/services/angular1.polyfills.ts
@@ -2,26 +2,35 @@ const win = window as any;
 
 win.Application = win.angular = {};
 
-const ANGULAR1_DEPRECATED = () => {
-    console.warn('DEPRECATED Angular 1.x feature');
-    return win.angular;
+const deprecatedAngularJSFeature = (feature) => {
+    return () => {
+        console.warn(`DEPRECATED AngularJS 1.x feature: ${feature}`);
+        return win.angular;
+    };
 };
 
-win.angular.module = ANGULAR1_DEPRECATED;
-win.angular.controller = ANGULAR1_DEPRECATED;
-win.angular.directive = ANGULAR1_DEPRECATED;
-win.angular.run = ANGULAR1_DEPRECATED;
-win.angular.config = ANGULAR1_DEPRECATED;
-win.angular.service = ANGULAR1_DEPRECATED;
-win.angular.factory = ANGULAR1_DEPRECATED;
-win.angular.constant = ANGULAR1_DEPRECATED;
-win.angular.value = ANGULAR1_DEPRECATED;
-win.angular.animation = ANGULAR1_DEPRECATED;
-win.angular.component = ANGULAR1_DEPRECATED;
-win.angular.decorator = ANGULAR1_DEPRECATED;
-win.angular.filter = ANGULAR1_DEPRECATED;
-win.angular.info = ANGULAR1_DEPRECATED;
-win.angular.provider = ANGULAR1_DEPRECATED;
-win.angular.requires = ANGULAR1_DEPRECATED;
+[
+    'module',
+    'controller',
+    'directive',
+    'run',
+    'config',
+    'service',
+    'factory',
+    'constant',
+    'value',
+    'animation',
+    'component',
+    'decorator',
+    'filter',
+    'info',
+    'provider',
+    'requires',
+    '$watch'
+].forEach(feature => {
+    win.angular[feature] = deprecatedAngularJSFeature(feature);
+});
+
+
 
 export const patchAngular1Deprecations = () => {};
