4d88029f7282109b26eb7aed70ad34b7a108ab43
Swathi <swathi.anupuram@wavemaker.com>
2018-07-27 14:08:46 +0530

    Added documentation for Html widget
    Change-Id: Ie006f4556d81a385a4f60e718978695ce746c5e9

---
+++ components/src/widgets/common/html/html.directive.doc.ts
@@ -0,0 +1,77 @@
+import { Input } from '@angular/core';
+
+/**
+ * The wmHtml component defines the html widget.
+ */
+export class Html {
+    /**
+     * Class of the widget.
+     */
+    @Input() class: string;
+    /**
+     * Content of the html widget.<br>
+     * <p><em>Bindable: </em><code>true</code></p>
+     */
+    @Input() content: string;
+    /**
+     * Title/hint for the html widget. <br>
+     * <p><em>Bindable: </em><code>true</code></p>
+     */
+    @Input() hint: string;
+    /**
+     * This property specifies how the elements should be aligned horizontally. <br>
+     * <p><em>Allowed Values: </em><code>left, center, right</code></p>
+     * <div class="summary">
+     * <p><code>left</code><em>: Aligns an element to the left.</em></p>
+     * <p><code>center</code><em>: Aligns an element to the center.</em></p>
+     * <p><code>right</code><em>:  Aligns an element to the right.</em></p>
+     * </div>
+     */
+    @Input() horizontalalign: string;
+    /**
+     * Name of the html widget.
+     */
+    @Input() name: string;
+    /**
+     * This property will be used to show/hide the html widget on the web page. <br>
+     * <p><em>Bindable: </em><code>true</code></p>
+     */
+    @Input() show: boolean = true;
+
+    /**
+     * Callback function which will be triggered when the widget is clicked.
+     * @param $event  DOM event on which call back is triggered
+     * @param widget  Instance of the html widget
+     */
+    click($event: MouseEvent, widget: any) {}
+    /**
+     * Callback function which will be triggered when the widget is double clicked.
+     * @param $event  DOM event on which call back is triggered
+     * @param widget  Instance of the html widget
+     */
+    dblclick($event: MouseEvent, widget: any) {}
+    /**
+     * Callback function which will be triggered when the mouse enters the widget.
+     * @param $event  DOM event on which call back is triggered
+     * @param widget  Instance of the html widget
+     */
+    mouseenter($event: MouseEvent, widget: any) {}
+    /**
+     * Callback function which will be triggered when the mouse leaves the widget.
+     * @param $event  DOM event on which call back is triggered
+     * @param widget  Instance of the html widget
+     */
+    mouseleave($event: MouseEvent, widget: any) {}
+    /**
+     * Callback function which will be triggered the tap event is triggered on a widget.
+     * @param $event  DOM event on which call back is triggered
+     * @param widget  Instance of the html widget
+     */
+    tap($event: MouseEvent, widget: any) {}
+    /**
+     * Callback function which will be triggered when the double tap event is triggered on a widget.
+     * @param $event  DOM event on which call back is triggered
+     * @param widget  Instance of the html widget
+     */
+    doubletap($event: MouseEvent, widget: any) {}
+}
