0338505a114f6ba715a164bff5a0685117675d10
abhilashsandi <abhilash.sandi@wavemaker.com>
2018-07-31 19:05:07 +0530

    Live table: Documentation
    Change-Id: Ib242808fb175db780f7f01b2723101fa446c8877

---
+++ components/src/widgets/common/live-table/live-table.component.doc.ts
@@ -0,0 +1,28 @@
+import { Input } from '@angular/core';
+
+/**
+ * The `wmLivetable` is the Combination of datatable and live form. Using Livetable user can insert,update,delete the data in database wmLivetable can be bound to variables and display the data associated with them.
+ */
+export class LiveTable {
+    /**
+     * Class of the widget.
+     */
+    @Input() class: string;
+    /**
+     * Name of the widget.
+     */
+    @Input() name: string;
+    /**
+     * This property will be used to show/hide the widget on the web page.
+     * <p><em>Bindable: </em><code>true</code></p>
+     */
+    @Input() show: boolean;
+    /**
+     * This property controls how the form appears.
+     * <p><em>Allowed Values: </em><code>inline, dialog</code></p>
+     * <div class="summary">
+     * <p><code>inline</code><em>: The form is displayed below table</em></p>
+     * <p><code>dialog</code><em>: The form is opened in dialog mode.</em></p>
+     */
+    @Input() formlayout: string = 'inline';
+}
