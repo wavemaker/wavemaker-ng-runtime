eaa8144f9923befacd1e21cd1d0bcea6b75601da
sampathg9 <sampath.gembali@wavemaker.com>
2018-07-31 13:29:03 +0530

    Added documentaion for prefab and prefab-container widgets
    Change-Id: I719d51bd09c94ffcb3b26acf08edf107c5e299f4

---
+++ components/src/widgets/common/prefab/prefab.component.doc.ts
@@ -0,0 +1,43 @@
+import { Input } from '@angular/core';
+
+/**
+ * The wmPrefab component defines the prefab widget.
+ */
+export class Prefab {
+
+    /**
+     * Class of the widget.
+     */
+    @Input() class: string;
+
+    /**
+     * Name(Unique across the web page) of the prefab widget.
+     */
+    @Input() name: string;
+
+    /**
+     * This property will be used to show/hide the prefab widget on the web page. <br>
+     * <p><em>Bindable: </em><code>true</code></p>
+     */
+    @Input() show: boolean = true;
+
+    /**
+     * Name of prefab to be rendered inside the prefab widget
+     */
+    @Input() prefabname: string;
+
+    /**
+     * Callback function which will be triggered when the widget is loading.
+     * @param $event  DOM event on which call back is triggered
+     * @param widget  Instance of the prefab widget
+     */
+    load($event: MouseEvent, widget: any) {}
+
+    /**
+     * Callback function which will be triggered when the widget is being destroyed.
+     * @param $event  DOM event on which call back is triggered
+     * @param widget  Instance of the prefab widget
+     */
+    destroy($event: MouseEvent, widget: any) {}
+    
+}
