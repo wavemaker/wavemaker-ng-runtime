36e4ec234f584bbdd7705c818b07ae94afd76490
Punith Mitra <punith.mitra@wavemaker.com>
2018-08-03 15:09:50 +0530

    Fix for [WMS-13755] : Panel, Accordian widgets header is not shown when the content is set to partial. (Partial is not loaded at specified target in the template).
    Change-Id: Iab7f2ce1eba2119eda68abf0b8dfb7805d192644

--- runtime/src/components/partial-container/partial-container.directive.ts
+++ runtime/src/components/partial-container/partial-container.directive.ts
@@ -22,18 +22,20 @@ export class PartialContainerDirective {
     }
 
     _renderPartial(nv) {
-
-        $invokeWatchers(true);
-
-        return this.partialRenderer.render(
-            nv,
-            this.vcRef,
-            this.$target,
-            this.componentInstance,
-            this.inj
-        ).then(() => {
-            this.contentInitialized = true;
-            this.onLoadSuccess();
+        // when the container-target is inside the component template, it can be queried after viewInit of the component.
+        setTimeout(() => {
+            this.$target = this.elRef.nativeElement.querySelector('[partial-container-target]') || this.elRef.nativeElement;
+            $invokeWatchers(true);
+            return this.partialRenderer.render(
+                nv,
+                this.vcRef,
+                this.$target,
+                this.componentInstance,
+                this.inj
+            ).then(() => {
+                this.contentInitialized = true;
+                this.onLoadSuccess();
+            });
         });
     }
 
