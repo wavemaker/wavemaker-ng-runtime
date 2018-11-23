7cc13a230cb0c8de38bf7fcd13c5824baaea35cf
kavyasree <kavya.sree@wavemaker.com>
2018-11-23 18:23:25 +0530

    DataTable: Restricting 'e' in input type for pagination and hightlighting the input on error.
    Change-Id: I36a822db4abf66d57318b213ded634073c6e42e9

--- components/src/widgets/common/pagination/pagination.component.ts
+++ components/src/widgets/common/pagination/pagination.component.ts
@@ -333,6 +333,7 @@ export class PaginationComponent extends StylableComponent implements AfterViewI
      In case of invalid input, navigate to the appropriate page; also return false.
      In case of valid input, return true.*/
     validateCurrentPage(event, callback?) {
+        const targetEle = $(event.target).closest('a');
         /*If the value entered is not a valid number, then navigate to the first page.*/
         if (isNaN(this.dn.currentPage)) {
             this.goToFirstPage(undefined, event, callback);
@@ -344,8 +345,8 @@ export class PaginationComponent extends StylableComponent implements AfterViewI
             return false;
         }
         /*If the value entered is greater than the last page number, then navigate to the last page.*/
-        if (this.pageCount && (this.dn.currentPage > this.pageCount)) {
-            this.goToLastPage(undefined, event, callback);
+        if (this.pageCount && (this.dn.currentPage > this.pageCount || _.isNull(this.dn.currentPage))) {
+            targetEle.addClass('ng-invalid');
             return false;
         }
         return true;
@@ -356,6 +357,16 @@ export class PaginationComponent extends StylableComponent implements AfterViewI
             return;
         }
         this.goToPage(event);
+    }
+
+    onKeyDown(event) {
+        const targetEle = $(event.target).closest('a');
+        if (event.code == 'KeyE') {
+            targetEle.addClass('ng-invalid');
+            return false;
+        }
+        targetEle.removeClass('ng-invalid');
+        return true;
     }
 
     pageChanged(event: any) {
