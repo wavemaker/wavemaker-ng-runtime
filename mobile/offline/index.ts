ef3fc2495bed3841c7328b98c8a42b9ad3154322
Bandhavya Vakiti <bandhavya.vakiti@wavemaker.com>
2018-12-11 11:54:11 +0530

    Fix for issue: mobile full-screen view, on selecting value from dropdown, while other query calls are running close search is triggered and the response for query is returned as [], and model is set to undefined and not selected value. - exposing local db service
    Change-Id: I4025d3bb21f3afd519b877c355e49f6501c1109d
    (cherry picked from commit aee50ec11fa1c9e3196bd3610034f2df185d2068)

--- mobile/offline/index.ts
+++ mobile/offline/index.ts
@@ -1,4 +1,5 @@
 export * from './src/offline.module';
 export * from './src/services/change-log.service';
+export * from './src/services/local-db.service';
 export * from './src/services/local-db-data-pull.service';
 export * from './src/services/local-db-management.service';
