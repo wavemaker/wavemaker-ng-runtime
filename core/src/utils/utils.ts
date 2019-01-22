05f5a3b5ca930cdf41ce40abda913212c003c3df
abhilashsandi <abhilash.sandi@wavemaker.com>
2019-01-22 18:18:36 +0530

    Form: Associate labels with form controls
    Change-Id: Iccdf99a54ab83daa5e3e6fe9f2b29b5606c5e1a8

--- core/src/utils/utils.ts
+++ core/src/utils/utils.ts
@@ -3,6 +3,7 @@ import { Subject } from 'rxjs';
 import { $watch } from './watcher';
 import { DataType } from '../enums/enums';
 import { DataSource } from '../types/types';
+import { setAttr } from './dom';
 
 declare const _, X2JS, _WM_APP_PROPERTIES;
 declare const moment;
@@ -1126,5 +1127,22 @@ const DEFAULT_DISPLAY_FORMATS = {
 // This method returns the display date format for given type
 export const getDisplayDateTimeFormat = type => {
     return DEFAULT_DISPLAY_FORMATS[_.toUpper(type)];
+};
+
+// Generate for attribute on label and ID on input element, so that label elements are associated to form controls
+export const addForIdAttributes = (element: HTMLElement) => {
+    const labelEl = element.querySelectorAll('label.control-label');
+    let inputEl = element.querySelectorAll('[focus-target]');
+    if (!inputEl.length) {
+        inputEl = element.querySelectorAll('input, select, textarea');
+    }
+    /*if there are only one input el and label El add id and for attribute*/
+    if (labelEl.length && inputEl.length) {
+        const widgetId = $(inputEl[0] as HTMLElement).closest('[widget-id]').attr('widget-id');
+        if (widgetId) {
+            setAttr(inputEl[0] as HTMLElement, 'id', widgetId);
+            setAttr(labelEl[0] as HTMLElement, 'for', widgetId);
+        }
+    }
 };
 
