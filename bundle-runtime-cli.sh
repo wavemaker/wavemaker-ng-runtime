939d5556bd1b439bfa160a086728e682d8bdf47b
Srujan <srujan.thammisetty@wavemaker.com>
2019-01-23 16:04:56 +0530

    Updated working directory path in bundle-runtime-cli.sh
    Change-Id: Iabece40cd1ccf5bdc7694eb6c417b634eea3e621

--- bundle-runtime-cli.sh
+++ bundle-runtime-cli.sh
@@ -1,20 +1,22 @@
 #!/usr/bin/env bash
 
+BASEDIR=$(dirname "$0")
+
 #copy datatable to libraries
-mkdir -p ./libraries/datatable/
-cp ./projects/components/src/widgets/common/table/datatable.js ./libraries/datatable/
+mkdir -p ${BASEDIR}/libraries/datatable/
+cp ${BASEDIR}/projects/components/src/widgets/common/table/datatable.js ${BASEDIR}/libraries/datatable/
 
-./node_modules/.bin/rollup -c ./rollup.build-task.js
+${BASEDIR}/node_modules/.bin/rollup -c ${BASEDIR}/rollup.build-task.js
 
-./node_modules/.bin/rimraf dist/runtime-cli
+${BASEDIR}/node_modules/.bin/rimraf dist/runtime-cli
 
-mkdir -p dist/runtime-cli
+mkdir -p ${BASEDIR}/dist/runtime-cli
 
-mkdir -p dist/runtime-cli/angular-app
-mkdir -p dist/runtime-cli/dependencies
+mkdir -p ${BASEDIR}/dist/runtime-cli/angular-app
+mkdir -p ${BASEDIR}/dist/runtime-cli/dependencies
 
-cp -r ./src dist/runtime-cli/angular-app
-cp -r ./libraries/ dist/runtime-cli/angular-app
-cp angular.json package.json package-lock.json tsconfig.json tsconfig.web-app.json dist/runtime-cli/angular-app
+cp -r ${BASEDIR}/src ${BASEDIR}/dist/runtime-cli/angular-app
+cp -r ${BASEDIR}/libraries/ ${BASEDIR}/dist/runtime-cli/angular-app
+cp ${BASEDIR}/angular.json ${BASEDIR}/package.json ${BASEDIR}/package-lock.json ${BASEDIR}/tsconfig.json ${BASEDIR}/tsconfig.web-app.json ${BASEDIR}/dist/runtime-cli/angular-app
 
-cp dist/transpilation/transpilation-web.cjs.js dist/transpilation/transpilation-mobile.cjs.js dist/runtime-cli/dependencies
+cp ${BASEDIR}/dist/transpilation/transpilation-web.cjs.js ${BASEDIR}/dist/transpilation/transpilation-mobile.cjs.js ${BASEDIR}/dist/runtime-cli/dependencies
