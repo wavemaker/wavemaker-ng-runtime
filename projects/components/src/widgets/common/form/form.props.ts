17c17df1f1915dcd7408e68fbf0b42d04a01cc87
swathi <swathi.anupuram@wavemaker.com>
2019-02-12 21:14:14 +0530

    Form Widget: Form properties are not registering in production build
    Change-Id: I10f3435f3017f3c5038fd0dd58aa7429962d00f3

--- projects/components/src/widgets/common/form/form.props.ts
+++ projects/components/src/widgets/common/form/form.props.ts
@@ -23,59 +23,65 @@ const propsMap = new Map(
     ]
 );
 
-if (isMobileApp()) {
-    propsMap.set('captionwidth', {value: 'xs-4 sm-4 md-4 lg-4', ...PROP_STRING});
-}
-
-const formMap = new Map(propsMap);
-formMap.set('action', PROP_STRING);
-formMap.set('defaultmode', {value: 'Edit', ...PROP_STRING});
-formMap.set('metadata', PROP_STRING);
-formMap.set('method', PROP_STRING);
-formMap.set('postmessage', {value: 'Data posted successfully', ...PROP_STRING});
-formMap.set('target', PROP_STRING);
-formMap.set('enctype', PROP_STRING);
-formMap.set('errormessage', {value: 'An error occured. Please try again!', ...PROP_STRING});
-formMap.set('messagelayout', {value: 'Inline', ...PROP_STRING});
-formMap.set('formdata', PROP_STRING);
-formMap.set('novalidate', PROP_BOOLEAN);
-formMap.set('validationtype', {value: 'default', ...PROP_STRING});
-formMap.set('iconclass', PROP_STRING);
 
 export const registerFormProps = () => {
+    if (isMobileApp()) {
+        propsMap.set('captionwidth', {value: 'xs-4 sm-4 md-4 lg-4', ...PROP_STRING});
+    }
+    const formMap = new Map(propsMap);
+    formMap.set('action', PROP_STRING);
+    formMap.set('defaultmode', {value: 'Edit', ...PROP_STRING});
+    formMap.set('metadata', PROP_STRING);
+    formMap.set('method', PROP_STRING);
+    formMap.set('postmessage', {value: 'Data posted successfully', ...PROP_STRING});
+    formMap.set('target', PROP_STRING);
+    formMap.set('enctype', PROP_STRING);
+    formMap.set('errormessage', {value: 'An error occured. Please try again!', ...PROP_STRING});
+    formMap.set('messagelayout', {value: 'Inline', ...PROP_STRING});
+    formMap.set('formdata', PROP_STRING);
+    formMap.set('novalidate', PROP_BOOLEAN);
+    formMap.set('validationtype', {value: 'default', ...PROP_STRING});
+    formMap.set('iconclass', PROP_STRING);
+
     register(
         'wm-form',
         formMap
     );
 };
 
-const liveFormMap = new Map(propsMap);
-liveFormMap.set('defaultmode', {value: 'View', ...PROP_STRING});
-liveFormMap.set('formlayout', {value: 'inline', ...PROP_STRING});
-liveFormMap.set('insertmessage', {value: 'Record added successfully', ...PROP_STRING});
-liveFormMap.set('updatemessage', {value: 'Record updated successfully', ...PROP_STRING});
-liveFormMap.set('deletemessage', {value: 'Record deleted successfully', ...PROP_STRING});
-liveFormMap.set('errormessage', {value: 'An error occured. Please try again!', ...PROP_STRING});
-liveFormMap.set('messagelayout', {value: 'Toaster', ...PROP_STRING});
-liveFormMap.set('formdata', PROP_STRING);
-liveFormMap.set('novalidate', PROP_BOOLEAN);
-liveFormMap.set('validationtype', {value: 'default', ...PROP_STRING});
-liveFormMap.set('iconclass', PROP_STRING);
-
 export const registerLiveFormProps = () => {
+    if (isMobileApp()) {
+        propsMap.set('captionwidth', {value: 'xs-4 sm-4 md-4 lg-4', ...PROP_STRING});
+    }
+    const liveFormMap = new Map(propsMap);
+    liveFormMap.set('defaultmode', {value: 'View', ...PROP_STRING});
+    liveFormMap.set('formlayout', {value: 'inline', ...PROP_STRING});
+    liveFormMap.set('insertmessage', {value: 'Record added successfully', ...PROP_STRING});
+    liveFormMap.set('updatemessage', {value: 'Record updated successfully', ...PROP_STRING});
+    liveFormMap.set('deletemessage', {value: 'Record deleted successfully', ...PROP_STRING});
+    liveFormMap.set('errormessage', {value: 'An error occured. Please try again!', ...PROP_STRING});
+    liveFormMap.set('messagelayout', {value: 'Toaster', ...PROP_STRING});
+    liveFormMap.set('formdata', PROP_STRING);
+    liveFormMap.set('novalidate', PROP_BOOLEAN);
+    liveFormMap.set('validationtype', {value: 'default', ...PROP_STRING});
+    liveFormMap.set('iconclass', PROP_STRING);
+
     register(
         'wm-liveform',
         liveFormMap
     );
 };
 
-const liveFilterMap = new Map(propsMap);
-liveFilterMap.set('autoupdate', PROP_BOOLEAN);
-liveFilterMap.set('enableemptyfilter', {value: ' ', ...PROP_STRING});
-liveFilterMap.set('pagesize', {value: 20, ...PROP_NUMBER});
-liveFilterMap.set('iconclass', {value: 'wi wi-filter-list', ...PROP_STRING});
-
 export const registerLiveFilterProps = () => {
+    if (isMobileApp()) {
+        propsMap.set('captionwidth', {value: 'xs-4 sm-4 md-4 lg-4', ...PROP_STRING});
+    }
+    const liveFilterMap = new Map(propsMap);
+    liveFilterMap.set('autoupdate', PROP_BOOLEAN);
+    liveFilterMap.set('enableemptyfilter', {value: ' ', ...PROP_STRING});
+    liveFilterMap.set('pagesize', {value: 20, ...PROP_NUMBER});
+    liveFilterMap.set('iconclass', {value: 'wi wi-filter-list', ...PROP_STRING});
+
     register(
         'wm-livefilter',
         liveFilterMap
