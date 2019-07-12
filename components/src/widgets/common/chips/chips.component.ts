e6a3b4073f7656b90b960eafae06546090779428
Bandhavya Vakiti <bandhavya.vakiti@wavemaker.com>
2019-07-12 14:37:01 +0530

    Fix for issue: watchIdentifier is not passed in the watcher.
    Change-Id: Ief6695bdd6ba00e8916fa823abfcc1590c9ec186

--- components/src/widgets/common/chips/chips.component.ts
+++ components/src/widgets/common/chips/chips.component.ts
@@ -148,7 +148,7 @@ export class ChipsComponent extends DatasetAwareFormComponent implements OnInit,
             $unwatch(watchName);
             this.registerDestroyListener($watch(this.bindChipclass, this.viewParent, {item, $index}, (nv, ov) => {
                 this.applyItemClass(getConditionalClasses(nv, ov), $index);
-            }), watchName);
+            }, watchName));
         }
     }
 
