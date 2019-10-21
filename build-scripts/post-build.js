afd8aeda9b21dd26525c1de3c5d11298c2358d95
Subodh <subodh.kumar@wavemaker.com>
2019-10-21 22:30:03 +0530

    WMS-18008 | Optimize CSS Bundle output
    Change-Id: I84cd86b8f1a009cde1e98761e0870572cc7f9895

--- build-scripts/post-build.js
+++ build-scripts/post-build.js
@@ -3,6 +3,7 @@ const fs = require('fs-extra')
 const readFile = util.promisify(fs.readFile);
 const writeFile = util.promisify(fs.writeFile);
 const copyFile = util.promisify(fs.copyFile);
+const exec = util.promisify(require('child_process').exec);
 const cheerio = require(`cheerio`);
 const crypto = require(`crypto`);
 const opPath = `${process.cwd()}/dist/ng-bundle`;
@@ -25,23 +26,25 @@ const generateHash = async (filepath)=>{
         $('script').attr('defer', 'true');
         isProdBuild = fs.existsSync(`${process.cwd()}/dist/ng-bundle/wm-styles.css`);
         isDevBuild = fs.existsSync(`${process.cwd()}/dist/ng-bundle/wm-styles.js`);
-        try {
             if(isDevBuild){
                 $("head").append(
                     `<script> const WMStylesPath = "ng-bundle/wm-styles.js" </script>`
                 )    
             }
             if(isProdBuild){
+                const isOptimizeCss = $('meta[optimizecss]').length;
+                if(isOptimizeCss){
+                    console.log(`CSS Optimization Selected`);
+                    const {stdout,stderr} = await exec(`npm run optimizecss`);
+                    console.log(`Optimization Log | ${stdout}`);
+                    console.error(`Optimization Error | ${stderr}`);
+                }
                 const hash = await generateHash(`${opPath}/wm-styles.css`);
                 copyCssFiles(hash);
                 $("head").append(
                     `<script> const WMStylesPath = "ng-bundle/wm-styles.${hash}.css" </script>`
                 );
             }
-        }
-        catch(e){
-            console.error(`Error in PostBuild Script | CSS Hash generation failed | ${e}`);
-        }
         await writeFile(`./dist/index.html`, $.html());
     } catch (e) {
         console.error(`Error in Post ng build Script | ${e}`);
