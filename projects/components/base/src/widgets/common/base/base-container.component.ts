fa8560c70b121735919fd264567662043363a241
UdayAppam <uday.appam@wavemaker.com>
2020-06-19 09:41:26 +0530

    switch widget while toggling nativeElement undefined fix
    Change-Id: I0bb9a8657b0a35ce8635acf77b807c20eebd6396

--- projects/components/base/src/widgets/common/base/base-container.component.ts
+++ projects/components/base/src/widgets/common/base/base-container.component.ts
@@ -12,7 +12,7 @@ export abstract class BaseContainerComponent extends StylableComponent {
     private updateRedrawableComponents(widgets) {
         _.pickBy(widgets, widget => {
             // check for redrawable widget and whether it is already exist in reDrawableComponents query list or not
-            if (widget.nativeElement.hasAttribute('redrawable') && !_.includes(this.reDrawableComponents._results, widget)) {
+            if (widget.nativeElement && widget.nativeElement.hasAttribute('redrawable') && !_.includes(this.reDrawableComponents._results, widget)) {
                 this.reDrawableComponents._results.push(widget);
             }
             if (widget.Widgets) {
