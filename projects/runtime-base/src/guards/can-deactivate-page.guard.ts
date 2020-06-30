63cbd3273fda95ae68d19ec19f9ecaf39bccfdac
Swathi <swathi.anupuram@wavemaker.com>
2020-06-30 20:20:02 +0530

    canDeactivate Guard[WMS-19159, WMS-19160]: "onBeforePageLeave" event is not being triggered when we close browser window/tab in Angular Build
    Change-Id: I75703dfad30e7c4319f1740641b3496aeb5307d5

--- projects/runtime-base/src/guards/can-deactivate-page.guard.ts
+++ projects/runtime-base/src/guards/can-deactivate-page.guard.ts
@@ -1,39 +1,42 @@
 import {ActivatedRouteSnapshot, CanDeactivate, Resolve, RouterStateSnapshot} from '@angular/router';
 import {Injectable, Injector} from '@angular/core';
 import {Observable} from 'rxjs';
+import {noop} from '@wm/core';
 
 
 export interface CanComponentDeactivate  {
     canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
 }
 
-   // canDeactivate guard for preview/WM deployment
+/*
+    canDeactivate guard for preview/WM deployment.
+    [WMS-19159] - Moved CanDeactivate() from page-wrapper.component.ts to base-page.component.ts(which is common for both preview and angular)
+    because beforepageleave is not calling for browser/tab close in angularmode
+    here component is pagewrapper so adding custom implementation for route change
+ */
 @Injectable()
 export class CanDeactivatePageGuard implements CanDeactivate<CanComponentDeactivate> {
-    canDeactivate(component: CanComponentDeactivate, route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        return component.canDeactivate ? component.canDeactivate() : true;
-    }
+  canDeactivate(component: CanComponentDeactivate, route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+      function invokeCompDeactivate() {
+          let retVal;
+          // Calling onBeforePageLeave method present at page level
+          retVal = this.app.activePage && this.app.activePage.onBeforePageLeave();
+          // Calling onBeforePageLeave method present at app level only if page level method return true
+          // or if there is no page level method
+          if (retVal !== false ) {
+              retVal =  (this.app.onBeforePageLeave || noop)(this.app.activePageName, this.app.activePage);
+          }
+          return retVal === undefined ? true : retVal;
+      }
+      return invokeCompDeactivate.apply(component);
+  }
 }
 
-/*
-    [WMS-18847] - canDeactivate guard for angular deployment.
-    As component.canDeactivate() is not part of basePageComponent, custom implementation of canDeactivate
-    is added(implementation taken from page-wrapper.component.ts -> canDeactivate())
-*/
+
+//  [WMS-18847] - canDeactivate guard for angular deployment.
 @Injectable()
 export class CanDeactivateNgPageGuard implements CanDeactivate<CanComponentDeactivate> {
     canDeactivate(component: CanComponentDeactivate, route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
-        function invokeCompDeactivate() {
-            let retVal;
-            // Calling onBeforePageLeave method present at page level
-            retVal = this.onBeforePageLeave();
-            // Calling onBeforePageLeave method present at app level only if page level method return true
-            // or if there is no page level method
-            if (retVal !== false ) {
-                retVal =  (this.App.onBeforePageLeave || this.noop)(this.App.activePageName, this.App.activePage);
-            }
-            return retVal === undefined ? true : retVal;
-        }
-        return invokeCompDeactivate.apply(component);
+        return component.canDeactivate ? component.canDeactivate() : true;
     }
 }
