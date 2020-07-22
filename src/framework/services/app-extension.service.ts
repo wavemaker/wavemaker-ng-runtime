cff6d2bee9a25a7d4652c15f33c5dc17ca92c4b9
UdayAppam <uday.appam@wavemaker.com>
2020-07-22 11:03:03 +0530

    formatter.js to be part of build
    Change-Id: I86484f5d15dc56b5c1cabe141b0c8441f5c0db37

--- src/framework/services/app-extension.service.ts
+++ src/framework/services/app-extension.service.ts
@@ -1,9 +1,8 @@
 import { Injectable } from '@angular/core';
 
 import { AppExtensionProvider } from '@wm/runtime/base';
-import { appendScriptToHead } from '@wm/core';
 
-
+declare const  WM_CUSTOM_FORMATTERS;
 @Injectable({
     providedIn: 'root'
 })
@@ -11,7 +10,7 @@ export class AppExtensionProviderService extends AppExtensionProvider {
 
     public loadFormatterConfigScript(callback:Function):void {
         try{
-            appendScriptToHead(callback);
+            callback(WM_CUSTOM_FORMATTERS);
 
     }catch(e){
         console.warn('Error while loading the formatters.js file');
