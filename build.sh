db4481d570b63f54c6c7d6ab6ac8c47af91ee85d
Subodh Kumar ES <subodh.kumar@wavemaker.com>
2020-08-11 17:11:57 +0530

    build changes
    Change-Id: I871613035674acb2d0dec5d65fe3b3b780d62bb2

--- build.sh
+++ build.sh
@@ -95,7 +95,7 @@ hasSourceChanges() {
         return 0
     fi
 
-    local updateTime=`gfind ${sourceLocation} -type f \( -name "*.ts" ! -name "*.doc.ts"  -o -name "*.html" \) -printf "%T@\n" | sort | tail -1 | cut -d. -f1`
+    local updateTime=`find ${sourceLocation} -type f \( -name "*.ts" ! -name "*.doc.ts"  -o -name "*.html" \) -printf "%T@\n" | sort | tail -1 | cut -d. -f1`
     local buildTime=`date -r ${successFile} +%s`
 
 	if [[ ${updateTime} -le ${buildTime} ]]; then
