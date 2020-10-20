f825bb95e793f1733d0bc445355f29a6e472dc5c
Swathi <swathi.anupuram@wavemaker.com>
2020-10-20 21:01:28 +0530

    WMS-19381: Added auto-close property for the color picker widget and exposed open and close methods
    Change-Id: I1192ae24a757ca510dce95dd7c67141414b42b36

--- projects/components/widgets/input/color-picker/src/color-picker.component.ts
+++ projects/components/widgets/input/color-picker/src/color-picker.component.ts
@@ -1,8 +1,9 @@
 import { Component, ElementRef, Injector, ViewChild } from '@angular/core';
 import { NgModel, NG_VALUE_ACCESSOR, NG_VALIDATORS } from '@angular/forms';
+import { ColorPickerDirective} from 'ngx-color-picker';
 
 import { addClass, removeClass } from '@wm/core';
-import { IWidgetConfig, provideAs, provideAsWidgetRef, styler } from '@wm/components/base';
+import { AUTOCLOSE_TYPE, IWidgetConfig, provideAs, provideAsWidgetRef, styler } from '@wm/components/base';
 import { BaseFormCustomComponent } from '@wm/components/input';
 import { registerProps } from './color-picker.props';
 
@@ -33,9 +34,13 @@ export class ColorPickerComponent extends BaseFormCustomComponent {
     public placeholder: any;
     public tabindex: any;
     public shortcutkey: string;
+    public outsideclick: boolean;
+    public autoclose: string;
+
 
     @ViewChild(NgModel) ngModel: NgModel;
     @ViewChild('input', { static: true, read: ElementRef }) inputEl: ElementRef;
+    @ViewChild(ColorPickerDirective) cpDirective: ColorPickerDirective;
 
     constructor(inj: Injector) {
         super(inj, WIDGET_CONFIG);
@@ -47,6 +52,25 @@ export class ColorPickerComponent extends BaseFormCustomComponent {
         const colorPickerContainer = this.nativeElement.querySelector(`.color-picker`) as HTMLElement;
         (!isOpen) ? addClass(colorPickerContainer, 'hidden') : removeClass(colorPickerContainer, 'hidden');
     }
+
+    colorPickerOpen(value: string) {
+        // Whenever autoclose property is set to 'always', adding the onclick listener to the colorPicker container to close the picker.
+        if (this.autoclose === AUTOCLOSE_TYPE.ALWAYS) {
+            const colorPickerContainer = this.nativeElement.querySelector(`.color-picker`) as HTMLElement;
+            colorPickerContainer.onclick = () => this.cpDirective.closeDialog();
+        }
+    }
+
+    // This mehtod is used to show/open the colorpicker popup.
+    open() {
+        this.cpDirective.openDialog();
+    }
+
+    // This mehtod is used to hide/close the colorpicker popup.
+    close() {
+        this.cpDirective.closeDialog();
+    }
+
 
     // change and blur events are added from the template
     protected handleEvent(node: HTMLElement, eventName: string, callback: Function, locals: any) {
@@ -63,14 +87,17 @@ export class ColorPickerComponent extends BaseFormCustomComponent {
         if (key === 'tabindex') {
             return;
         }
-        if(key === 'required') {
+        if (key === 'required') {
             /* WMS-18269 | Update Angular about the required attr value change */
             this._onChange(this.datavalue);
             return;
         }
-        if(key === 'datavalue') {
+        if (key === 'datavalue') {
             this._onChange(nv);
             return;
+        }
+        if (key === 'autoclose') {
+            this.outsideclick = (nv === AUTOCLOSE_TYPE.OUTSIDECLICK || nv === AUTOCLOSE_TYPE.ALWAYS) ? true : false;
         }
         super.onPropertyChange(key, nv, ov);
     }
