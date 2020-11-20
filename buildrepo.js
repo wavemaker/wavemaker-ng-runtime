cd72f6ef1de54f97399f5670f18a3561ab4a4cd1
Uday <uday.appam@wavemaker.com>
2020-11-20 21:44:59 +0530

    build node file added
    Change-Id: I6e2c4f3fed6d81a410c19a41de8ef0237fd89d5e

---
+++ buildrepo.js
@@ -0,0 +1,12 @@
+const { exec } = require("child_process");
+exec("sh build.sh", (error, stdout, stderr) => {
+    if (error) {
+        console.log(`error: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.log(`stderr: ${stderr}`);
+        return;
+    }
+    console.log(`stdout: ${stdout}`);
+});
