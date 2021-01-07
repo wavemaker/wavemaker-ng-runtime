03c87814d48c53f703b033fcf8ccee33ce20dfe7
aparna.jayanthi <aparna.jayanthi@wavemaker.com>
2021-01-07 16:28:51 +0530

    FormWidget[WMS-19904]: mark the field as touched only on user triggered input

--- projects/components/widgets/data/form/src/form-field/form-field.directive.ts
+++ projects/components/widgets/data/form/src/form-field/form-field.directive.ts
@@ -1,4 +1,4 @@
-import { AfterContentInit, Attribute, ContentChild, Directive, Inject, Injector, OnInit, Optional, Self } from '@angular/core';
+import { AfterContentInit, Attribute, ContentChild, Directive, Inject, Injector, OnInit, Optional, Self, HostListener } from '@angular/core';
 import { FormBuilder, FormGroup, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 import { debounceTime } from 'rxjs/operators';
@@ -96,6 +96,11 @@ export class FormFieldDirective extends StylableComponent implements OnInit, Aft
     private _activeField: boolean;
     private notifyForFields: any;
     private fieldValidations;
+    private userTriggeredInput: boolean;
+
+    @HostListener('document:keydown', ['$event']) onKeydownHandler(event: KeyboardEvent) {
+        this.userTriggeredInput = true;
+    }
 
     constructor(
         inj: Injector,
@@ -159,6 +164,7 @@ export class FormFieldDirective extends StylableComponent implements OnInit, Aft
     _onBlurField($evt) {
         $($evt.target).closest('.live-field').removeClass('active');
         this._activeField = false;
+        this.userTriggeredInput = false;
     }
 
     // Expression to be evaluated in view mode of form field
@@ -377,7 +383,10 @@ export class FormFieldDirective extends StylableComponent implements OnInit, Aft
         if (!this.isDestroyed) {
             this.form.onFieldValueChange(this, val);
             this.notifyChanges();
-            if (this.$element.find('.active').length > 0 || this.form.touched) {
+            // Do mark as touched, only incase when user has entered an input but not through the script. Hence added mousedown event check
+            // active class checks whether user is on the current field, if so marking the field as touched. And form field validation happens once a field is touched
+            // userTriggeredInput checks whether the field is touched by the user or triggered from external script
+            if ((this.$element.find('.active').length > 0 && this.userTriggeredInput) || this.form.touched) {
                 this.ngform.controls[this._fieldName].markAsTouched();
                 this.fieldValidations.setCustomValidationMessage();
             }
