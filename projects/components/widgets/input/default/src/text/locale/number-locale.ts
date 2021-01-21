5b4df49223135a0ae06cee3b2010590840b378d0
Aparna Jayanthi <aparna.jayanthi@wavemaker.com>
2021-01-21 07:48:55 +0000

    NumberWidget: Fix for test case failure, added length check

--- projects/components/widgets/input/default/src/text/locale/number-locale.ts
+++ projects/components/widgets/input/default/src/text/locale/number-locale.ts
@@ -174,10 +174,10 @@ export abstract class NumberLocale extends BaseInput implements Validator {
         if (Number.isNaN(number) || Number.isNaN(decimal)) {
             return NaN;
         }
-        const precession = parts[1].length;
+        const sum = parts.length > 1 ? parseFloat((number + decimal).toFixed(parts[1].length)) : number + decimal;
         // if the number is negative then calculate the number as number - decimal
         // Ex: number = -123 and decimal = 0.45 then number - decimal = -123-045 = -123.45
-        return number >= 0 ? parseFloat((number + decimal).toFixed(precession)) : number - decimal;
+        return number >= 0 ? sum : number - decimal;
     }
 
     // updates the widgets text value.
