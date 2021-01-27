32fd0c422ba564bb376c916b5af269b1b30369d2
Uday Appam <uday.appam@wavemaker.com>
2021-01-27 06:32:38 +0000

    Partial param fix for the datewidgets
    Change-Id: I485ee3f9417f0f688e4587b1586a19d0116978fa

--- projects/components/widgets/input/epoch/src/date/date.component.ts
+++ projects/components/widgets/input/epoch/src/date/date.component.ts
@@ -35,7 +35,8 @@ export class DateComponent extends BaseDateTimeComponent {
     private dateContainerCls: string;
     public isOpen: boolean = false;
     private isEnterPressedOnDateInput = false;
-
+    private _bsDefaultLoadCheck: boolean;
+   
     private keyEventPlugin;
     private deregisterEventListener;
 
@@ -89,7 +90,7 @@ export class DateComponent extends BaseDateTimeComponent {
         this.dateContainerCls = `app-date-${this.widgetId}`;
         this._dateOptions.containerClass = `app-date ${this.dateContainerCls}`;
         this._dateOptions.showWeekNumbers = false;
-
+        this._bsDefaultLoadCheck = true;
         this.datepattern = this.appDefaults.dateFormat || getDisplayDateTimeFormat(FormWidgetType.DATE);
         this.updateFormat('datepattern');
     }
@@ -243,6 +244,18 @@ export class DateComponent extends BaseDateTimeComponent {
      * This is an internal method triggered when the date selection changes
      */
     onDateChange(newVal): void {
+
+        /**
+         *  Ngx-bootstrap upgrade : To avoid the page load datechange event;
+         *  TODO:
+         *  https://github.com/valor-software/ngx-bootstrap/issues/6016
+         *  For above issue, once we get the solution from Ngx-Bootstrap team,  remove the _bsDefaultLoadCheck check and update accordingly. 
+         * */ 
+        if (this._bsDefaultLoadCheck) {
+            this._bsDefaultLoadCheck = false;
+            return;
+        }
+
         const displayInputElem = this.nativeElement.querySelector('.display-input') as HTMLElement;
         if (this.isOpen) {
             setTimeout(() => displayInputElem.focus());
