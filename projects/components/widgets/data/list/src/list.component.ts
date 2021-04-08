30683261cc6e5ba50d536f3ce70837b15f5df5eb
aparna.jayanthi <aparna.jayanthi@wavemaker.com>
2021-04-08 17:08:04 +0530

    ListWidget[WMS-20265]: Give preference to height and then modal

--- projects/components/widgets/data/list/src/list.component.ts
+++ projects/components/widgets/data/list/src/list.component.ts
@@ -808,10 +808,10 @@ export class ListComponent extends StylableComponent implements OnInit, AfterVie
     private configureDnD() {
         let appendTo;
         const modalEl = $(document).find('.modal');
-        if (modalEl.length) { // In case of dialog, appendTo should be the modal ele
+        if (this.getAttr('height')) { // when height is applied to the list, append should be the ul's parent as scroll is applied to the parent
+          appendTo = 'parent';
+        } else if (modalEl.length) { // In case of dialog, appendTo should be the modal ele
             appendTo = modalEl[modalEl.length - 1];
-        } else if (this.getAttr('height')) { // when height is applied to the list, append should be the ul's parent as scroll is applied to the parent
-            appendTo = 'parent';
         } else { // As default append to should be body
             appendTo = 'body';
         }
