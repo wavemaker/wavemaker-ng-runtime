815285eb327b08d3dbb7f2a024d5a2fc93327869
Radhakrishnan <radhakrishnan.asokan@wavemaker.com>
2021-04-09 13:28:07 +0530

    Scroll [WMS-20296] - Disabled Smooth for "Tab-pan"
    Change-Id: I00103826c13458736054fea23804a10eb12dbdc0

--- projects/components/widgets/containers/tabs/src/tab-pane/tab-pane.props.ts
+++ projects/components/widgets/containers/tabs/src/tab-pane/tab-pane.props.ts
@@ -18,7 +18,7 @@ export const registerProps = () => {
                 ['name', PROP_STRING],
                 ['paneicon', PROP_STRING],
                 ['show', {value: true, ...PROP_BOOLEAN}],
-                ['smoothscroll', {value: true, ...PROP_BOOLEAN}],
+                ['smoothscroll', {value: false, ...PROP_BOOLEAN}],
                 ['tabindex', {value: 0, ...PROP_NUMBER}],
                 ['title', {value: 'Tab Title', ...PROP_STRING}]
             ]
