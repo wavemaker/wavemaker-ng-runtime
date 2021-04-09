a3f44729d2f56ee7c8d495f13b3bc47fd38eeb9f
Aparna Jayanthi <aparna.jayanthi@wavemaker.com>
2021-04-09 06:34:39 +0000

    TableWidget[WMS-19776]: In case of on demand pagination, when new row is added show it in last page

--- projects/components/widgets/data/live-table/src/live-table.component.ts
+++ projects/components/widgets/data/live-table/src/live-table.component.ts
@@ -189,6 +189,9 @@ export class LiveTableComponent extends StylableComponent implements AfterConten
         this.form.isUpdateMode = isDefined(updateMode) ? updateMode : newForm ? true : false;
         switch (operation) {
             case 'insert':
+                if (this.table.navigation === 'On-Demand') {
+                    this.table.isNewRowInserted = true;
+                }
                 if (newForm) {
                     /*if new form is to be shown after insert, skip the highlight of the row*/
                     this.table.gridfirstrowselect = false;
