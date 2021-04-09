e61d7190a4a90b1f1d48e945e2858829de6719e4
Swathi <swathi.anupuram@wavemaker.com>
2021-04-09 15:28:50 +0530

    WMS-20160: Unit test case failures fix
    Change-Id: I88b5ea153d888081e20010ed4cfecaf5c1e6107a

--- projects/components/widgets/input/chips/src/chips.wrapper.component.spec.ts
+++ projects/components/widgets/input/chips/src/chips.wrapper.component.spec.ts
@@ -3,12 +3,13 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { TypeaheadModule } from 'ngx-bootstrap';
+import { DatePipe } from '@angular/common';
 
 import { App } from '@wm/core';
 import { ChipsComponent } from './chips.component';
 import { ComponentTestBase, ITestComponentDef, ITestModuleDef } from '../../../../base/src/test/common-widget.specs';
 import { compileTestComponent, setInputValue } from '../../../../base/src/test/util/component-test-util';
-import { WmComponentsModule } from '@wm/components/base';
+import { WmComponentsModule, ToDatePipe } from '@wm/components/base';
 import { SearchComponent } from '@wm/components/basic/search';
 import { PartialRefProvider } from '@wm/core';
 
@@ -38,6 +39,8 @@ const testModuleDef: ITestModuleDef = {
     declarations: [ChipsWrapperComponent, ChipsComponent, SearchComponent],
     providers: [
         { provide: App, useValue: mockApp },
+        { provide: ToDatePipe, useClass: ToDatePipe },
+        { provide: DatePipe, useClass: DatePipe },
         { provide: PartialRefProvider, useClass: PartialRefProvider }
     ]
 };
