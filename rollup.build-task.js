f661dd3ff29717378d9febb6f5125739ac3ea043
nareshr <naresh.ravulapalli@wavemaker.com>
2021-07-26 09:57:24 +0530

    WMS-20945 - wavemaker-studio-frontend repo upgrade. disabled rollup closure compiler. upgraded es5 to es6 for the application code generation. reverted all the rollup plugins to older versions.

--- rollup.build-task.js
+++ rollup.build-task.js
@@ -1,71 +1,56 @@
-import multiEntry from '@rollup/plugin-multi-entry';
-import { nodeResolve } from '@rollup/plugin-node-resolve';
-import commonJS from '@rollup/plugin-commonjs';
-import alias from '@rollup/plugin-alias';
+import multiEntry from 'rollup-plugin-multi-entry';
+import nodeResolve from 'rollup-plugin-node-resolve';
+import commonJS from 'rollup-plugin-commonjs';
+import alias from 'rollup-plugin-alias';
 import compiler from '@ampproject/rollup-plugin-closure-compiler';
-import { terser } from "rollup-plugin-terser";
 
 export default [
     {
-        input: './libraries/build-task/fesm2015/index.js',
+        input: 'libraries/build-task/fesm2015/index.js',
         output: {
-            file: './dist/transpilation/transpilation-web.cjs.js',
+            file: 'dist/transpilation/transpilation-web.cjs.js',
             format: 'cjs'
-        },
-        acorn: {
-            allowReserved: false
         },
         plugins: [
             alias({
-                entries: [
-                    {'find': 'rxjs/Subject', 'replacement': './node_modules/rxjs/_esm5/internal/Subject.js'},
-                    {'find': '@wm/core', 'replacement': './libraries/core/fesm2015/index.js'},
-                    {'find': '@wm/transpiler', 'replacement': './libraries/transpiler/fesm2015/index.js'}
-                ]
+                'rxjs/Subject': 'node_modules/rxjs/_esm5/internal/Subject.js',
+                '@wm/core': 'libraries/core/fesm2015/index.js',
+                '@wm/transpiler': 'libraries/transpiler/fesm2015/index.js'
             }),
             nodeResolve({
-                mainFields: ['jsnext', 'module', 'main'],
+                jsnext: true,
+                main: true
             }),
             commonJS({
-                include: './node_modules/**',
+                include: 'node_modules/**',
                 ignoreGlobal: true
-            }),
-            terser()
-            // compiler()
+            })
         ]
     },
     {
         input: [
-            './libraries/build-task/fesm2015/index.js',
-            './libraries/mobile-build-task/fesm2015/index.js'
+            'libraries/build-task/fesm2015/index.js',
+            'libraries/mobile-build-task/fesm2015/index.js'
         ],
         output: {
-            file: './dist/transpilation/transpilation-mobile.cjs.js',
+            file: 'dist/transpilation/transpilation-mobile.cjs.js',
             format: 'cjs'
-        },
-        acorn: {
-            allowReserved: true
         },
         plugins: [
             multiEntry(),
             alias({
-                entries: [
-                    {'find': 'rxjs/Subject', 'replacement': './node_modules/rxjs/_esm5/internal/Subject.js'},
-                    {'find': '@wm/core', 'replacement': './libraries/core/fesm2015/index.js'},
-                    {'find': '@wm/transpiler', 'replacement': './libraries/transpiler/fesm2015/index.js'}
-                ]
+                'rxjs/Subject': 'node_modules/rxjs/_esm5/internal/Subject.js',
+                '@wm/core': 'libraries/core/fesm2015/index.js',
+                '@wm/transpiler': 'libraries/transpiler/fesm2015/index.js'
             }),
             nodeResolve({
-                mainFields: ['module', 'main']
+                jsnext: true,
+                main: true
             }),
             commonJS({
-                include: 'node_modules/!**',
+                include: 'node_modules/**',
                 ignoreGlobal: true
-            }),
-            terser()
-            /*compiler({
-                formatting: 'PRETTY_PRINT'
-            })*/
+            })
         ]
     }
 ]
