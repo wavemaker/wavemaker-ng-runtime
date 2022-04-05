679900faa46fed525185c5e0ac848acbf3bc0d87
aparnas <aparna.jayanthi@wavemaker.com>
2022-04-05 12:59:15 +0530

    Revert "TreeWidget [WMS-22099]: Do not redraw widgets which are already in viewport"
    This reverts commit 2406f43c5d4027015352bbeceab2ce14c90b65be.

--- projects/components/base/src/widgets/common/redraw/redrawable.directive.ts
+++ projects/components/base/src/widgets/common/redraw/redrawable.directive.ts
@@ -2,8 +2,6 @@ import { Directive, Inject, Self } from '@angular/core';
 
 import { IRedrawableComponent, WidgetRef } from '../../framework/types';
 
-import { isElementInViewport } from '@wm/core';
-
 @Directive({
     selector: '[redrawable]'
 })
@@ -13,8 +11,7 @@ export class RedrawableDirective implements IRedrawableComponent {
         this.redraw = () => {
             const tabEl = widget.$element.closest("[wmtabpane]");
             const accordionEl = widget.$element.closest('[wmaccordionpane]').find('.panel-heading');
-            // WMS-22099: Do not apply redraw on widgets which are already in view
-            if (isElementInViewport(widget.$element[0]) || (tabEl.length && !tabEl.hasClass('active')) || (accordionEl.length && !accordionEl.hasClass('active'))) {
+            if ((tabEl.length && !tabEl.hasClass('active')) || (accordionEl.length && !accordionEl.hasClass('active'))) {
                 return;
             }
             return widget.redraw && widget.redraw();
