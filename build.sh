d89dd84146da069da5763e4709d78f2a55a98a13
nareshr <naresh.ravulapalli@wavemaker.com>
2022-05-10 08:14:44 +0530

    WMS-21842 - Angular Upgrade to 12.2.16. ng-12 build failure issues and corejs dependency issues resolved

--- build.sh
+++ build.sh
@@ -45,6 +45,7 @@ if [[ ${force} == true ]]; then
     ${RIMRAF} ./dist/
 fi
 
+mkdir -p ./dist/tmp/libs/core-js
 mkdir -p ./dist/bundles/wmapp/scripts
 mkdir -p ./dist/bundles/wmmobile/scripts
 
@@ -474,6 +475,10 @@ copyLocale() {
     fi
 }
 
+buildCoreJs() {
+    execCommand "build" "core-js" "node ./core-js-builder.js"
+}
+
 buildTsLib() {
     execCommand "rollup" "tslib" "${ROLLUP} ./node_modules/tslib/tslib.es6.js --o ./dist/tmp/libs/tslib/tslib.umd.js -f umd --name tslib --silent"
 }
@@ -508,6 +513,7 @@ bundleWebLibs() {
     echo "uglify: web-libs"
     ${TERSER} \
         ./dist/tmp/libs/tslib/tslib.umd.js \
+        ./dist/tmp/libs/core-js/core-js.umd.js \
         ./node_modules/zone.js/dist/zone.js \
         ./node_modules/rxjs/bundles/rxjs.umd.js \
         ./node_modules/@angular/core/bundles/core.umd.js \
@@ -572,6 +578,7 @@ bundleMobileLibs() {
     echo "uglify: mobile-libs"
     ${TERSER} \
         ./dist/tmp/libs/tslib/tslib.umd.js \
+        ./dist/tmp/libs/core-js/core-js.umd.js \
         ./node_modules/zone.js/dist/zone.js \
         ./node_modules/rxjs/bundles/rxjs.umd.js \
         ./node_modules/@angular/core/bundles/core.umd.js \
@@ -635,6 +642,7 @@ bundleMobileLibs() {
 }
 
 buildWebLibs() {
+    buildCoreJs
     buildTsLib
     buildNgxBootstrap
     buildNgxToastr
