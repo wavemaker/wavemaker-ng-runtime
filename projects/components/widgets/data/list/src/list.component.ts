21cc9353d3eed0e7b91c263781af81611a9ab1bb
aparnas <aparna.jayanthi@wavemaker.com>
2022-05-13 13:37:51 +0530

    ListWidget [WMS-17268]: Update nodatafound flag once the response is recieved from the server

--- projects/components/widgets/data/list/src/list.component.ts
+++ projects/components/widgets/data/list/src/list.component.ts
@@ -341,6 +341,9 @@ export class ListComponent extends StylableComponent implements OnInit, AfterVie
             this.ngZone.run(() => {
                 this.handleStateParams(data);
                 this.variableInflight = data.active;
+                if (this.noDataFound) {
+                    this.noDataFound = data.data ? false : true;
+                }
             });
         }
     }
