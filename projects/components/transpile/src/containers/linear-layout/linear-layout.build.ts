f4cbb5e9794cbce8726b503cf7e15e4324d9ee03
Srinivasa Rao Boyina <2254369+sboyina@users.noreply.github.com>
2022-05-24 18:59:03 +0530

    WMS-22151: Chages to inherit spacing from parent linear layout to child linear layouts.

--- projects/components/transpile/src/containers/linear-layout/linear-layout.build.ts
+++ projects/components/transpile/src/containers/linear-layout/linear-layout.build.ts
@@ -1,11 +1,22 @@
 import { getAttrMarkup, IBuildTaskDef, register } from '@wm/transpiler';
 
 const tagName = 'div';
-
+const SPACING_KEY = 'parentLinearLayout.spacing';
 register('wm-linearlayout', (): IBuildTaskDef => {
     return {
-        pre: attrs => `<${tagName} wmLinearLayout ${getAttrMarkup(attrs)}>`,
-        post: () => `</${tagName}>`
+        requires: ['wm-linearlayout'],
+        pre: (attrs, shared, provider) => {
+            let spacing = attrs.get('spacing');
+            attrs.set('spacing',
+                (!spacing || spacing === '0') && provider ? provider.get(SPACING_KEY) : spacing);
+            return `<${tagName} wmLinearLayout ${getAttrMarkup(attrs)}>`;
+        },
+        post: () => `</${tagName}>`,
+        provide: (attrs, shared) => {
+            const provider = new Map();
+            provider.set(SPACING_KEY, attrs.get('spacing'));
+            return provider;
+        }
     };
 });
 
