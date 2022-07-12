11b4132d3514021f884cd6e623413558c39f4c04
Srinivasa Rao Boyina <2254369+sboyina@users.noreply.github.com>
2022-07-12 20:49:23 +0530

    WMS-22569: Introduced page cache flag on navigation variable.

--- projects/variables/src/util/action/navigation-action.utils.ts
+++ projects/variables/src/util/action/navigation-action.utils.ts
@@ -1,5 +1,7 @@
 import { navigationService } from '../variable/variables.utils';
 
+declare const _;
+
 /**
  * Handles variable navigation operations
  * @param variable
@@ -10,9 +12,13 @@ export const navigate = (variable, options) => {
     let viewName;
     const pageName = variable.dataBinding.pageName || variable.pageName,
         operation = variable.operation,
-        urlParams = variable.dataSet;
-
+        urlParams = _.clone(variable.dataSet),
+        cacheable = _.isNil(options?.cacheable) ? variable.cacheable : options.cacheable;
     options = options || {};
+
+    if (!_.isNil(cacheable) && cacheable !== 'default') {
+        urlParams['_cache_page'] = cacheable;
+    }
 
     /* if operation is goToPage, navigate to the pageName */
     switch (operation) {
@@ -23,7 +29,8 @@ export const navigate = (variable, options) => {
             navigationService.goToPage(pageName, {
                 transition: variable.pageTransitions,
                 $event: options.$event,
-                urlParams: urlParams
+                urlParams: urlParams,
+                cacheable: variable.cacheable
             });
             break;
         case 'gotoView':
