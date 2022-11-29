743452730dd4c56ab4f40daaa523cea1f27c50be
Bhargavi Gunda <bhargavi.gunda@wavemaker.com>
2022-11-29 12:13:02 +0000

    WMS-23228: Pushing vulnerabilities to SonarQube

---
+++ sonarQube.js
@@ -0,0 +1,22 @@
+const scanner = require("sonarqube-scanner");
+
+scanner(
+    {
+        serverUrl: process.argv[2],
+        options: {
+            "sonar.login": process.argv[3],
+            "sonar.projectKey": process.argv[4],
+            "sonar.projectName": process.argv[4],
+            "sonar.projectVersion": process.argv[5],
+            "sonar.exclusions": "node_modules/**/*, dist/**/*, dependency-check-bin/**, dependency-check-reports/*",
+            "sonar.scm.enabled": "false",
+            "sonar.dependencyCheck.htmlReportPath": "./dependency-check-reports/dependency-check-report.html",
+            "sonar.dependencyCheck.jsonReportPath": "./dependency-check-reports/dependency-check-report.json",
+            "sonar.dependencyCheck.xmlReportPath": "./dependency-check-reports/dependency-check-report.xml",
+            "sonar.javascript.lcov.reportPaths": "coverage/lcov.info"
+        }
+    },
+    () => {
+        // A must callback
+    }
+);
