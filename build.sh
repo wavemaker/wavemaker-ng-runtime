5c2ae5fc0f088b2d74fe7d2c12dd0ad09f874bd0
suchethang_500160 <suchethang_500160@PSPLHYLTAP0083.local>
2022-12-15 11:00:24 +0530

    WMS-23086 | feature to trap focus within a dialog when we tab through the tabbable elements in it

--- build.sh
+++ build.sh
@@ -582,6 +582,8 @@ bundleWebLibs() {
         ./node_modules/angular-imask/bundles/angular-imask.umd.js \
         ./node_modules/@metrichor/jmespath/dist/jmespath.umd.js \
         ./dist/tmp/libs/ngx-bootstrap/ngx-bootstrap.umd.js \
+        ./node_modules/tabbable/dist/index.umd.js   \
+        ./node_modules/@wavemaker/focus-trap/dist/focus-trap.umd.js \
         -o ./dist/bundles/wmapp/scripts/wm-libs.js -b
 
          ./node_modules/.bin/terser ./dist/bundles/wmapp/scripts/wm-libs.js \
@@ -651,6 +653,8 @@ bundleMobileLibs() {
         ./node_modules/imask/dist/imask.min.js \
         ./node_modules/@metrichor/jmespath/dist/jmespath.umd.js \
         ./node_modules/angular-imask/bundles/angular-imask.umd.js \
+        ./node_modules/tabbable/dist/index.umd.js   \
+        ./node_modules/@wavemaker/focus-trap/dist/focus-trap.umd.js \
         -o ./dist/bundles/wmmobile/scripts/wm-libs.js -b
 
     ./node_modules/.bin/terser ./dist/bundles/wmmobile/scripts/wm-libs.js \
