28084e86ca64d4dc9763db2da185aefe8d420aed
Priyanka Patnala <priyanka.patnala@wavemaker.com>
2023-01-30 17:04:47 +0530

    fix for [WMS-23397] Create,Update failing due to Number widget issue due to update delay property changes

--- projects/components/widgets/input/default/src/text/base/base-input.ts
+++ projects/components/widgets/input/default/src/text/base/base-input.ts
@@ -56,21 +56,28 @@ export abstract class BaseInput extends BaseFormCustomComponent implements After
     // invoke the change callback
     handleChange(newValue: any) {
         // fix for [WMS-23157] Implementing Update Delay Property for text & textarea widgets
-        const debounce = _.toNumber(this.nativeElement.getAttribute('updatedelay')) || 0;
-        if (debounce === 0) {
-            this.datavalue = newValue;
-            if (this.autotrim && this.datavalue && _.isString(this.datavalue)) {
-                this.datavalue = this.datavalue.trim();
-            }
-            this.invokeOnChange(this.datavalue, {type: 'change'}, this.ngModel.valid);
-        } else {
-            setTimeout(() => {
+        if (this.widget.widgetType === 'wm-input-text' || this.widget.widgetType === 'wm-textarea') {
+            const debounce = _.toNumber(this.nativeElement.getAttribute('updatedelay')) || 0;
+            if (debounce === 0) {
                 this.datavalue = newValue;
                 if (this.autotrim && this.datavalue && _.isString(this.datavalue)) {
                     this.datavalue = this.datavalue.trim();
                 }
                 this.invokeOnChange(this.datavalue, {type: 'change'}, this.ngModel.valid);
-            }, debounce);        }
+            } else {
+                setTimeout(() => {
+                    this.datavalue = newValue;
+                    if (this.autotrim && this.datavalue && _.isString(this.datavalue)) {
+                        this.datavalue = this.datavalue.trim();
+                    }
+                    this.invokeOnChange(this.datavalue, {type: 'change'}, this.ngModel.valid);
+                }, debounce);        }
+        } else {
+            if (this.autotrim && this.datavalue && _.isString(this.datavalue)) {
+                this.datavalue = this.datavalue.trim();
+            }
+            this.invokeOnChange(this.datavalue, {type: 'change'}, this.ngModel.valid);
+        }
     }
 
     // Change event is registered from the template, Prevent the framework from registering one more event
