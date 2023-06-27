60c8fe388b0e52940e1c839757b61e4969987524
Bhargavi Gunda <bhargavi.gunda@wavemaker.com>
2023-06-27 12:07:53 +0530

    WMS-23956: Issue with List widget when pagination selected as On Demand

--- projects/core/src/services/pagination.service.ts
+++ projects/core/src/services/pagination.service.ts
@@ -24,7 +24,12 @@ export class PaginationService {
             fieldDefs = [];
             parent.gridOptions.setCurrentPage(1);
         }
-        let currentPage = parent.gridOptions.showviewlessbutton && (parent.gridOptions.mode === 'viewless') ? parent.gridOptions.getCurrentPage() :  parent.currentPage ;
+        let currentPage;
+        if (parent.widgetType === 'wm-table') {
+            currentPage = parent.gridOptions.showviewlessbutton && (parent.gridOptions.mode === 'viewless') ? parent.gridOptions.getCurrentPage() : parent.currentPage;
+        } else {
+            currentPage = parent.currentPage;
+        }
         const dataNavigator = parent.dataNavigator;
         const pagesize = parent.pagesize;
 
