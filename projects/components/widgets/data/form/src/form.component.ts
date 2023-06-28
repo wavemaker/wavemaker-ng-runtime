fbe6337c5683a60effe7d2376c134ba87eeb4147
PriyankaPatnala <priyanka.patnala@wavemaker.com>
2023-06-28 19:10:55 +0530

    Fix for ADA Issue [WMS-23959] - The wm-input element uses aria-describedby, but the attribute is referencing a non-existent id

--- projects/components/widgets/data/form/src/form.component.ts
+++ projects/components/widgets/data/form/src/form.component.ts
@@ -83,7 +83,7 @@ const setTouchedState = (self, ngForm, fieldName) => {
             setTouchedState(self, ctrl, fieldName);
         });
     } else {
-        const element = self.$element.find(`form.ng-invalid:visible,[wmformfield][key="${fieldName}"]`);
+        const element = self.$element.find(`[wmformfield][key="${fieldName}"]`);
         element[0].setAttribute('__errormsg', element[0].getAttribute('__validationId'));
         element[0].querySelector('input')?.setAttribute('aria-invalid', ngForm.invalid);
         element[0].querySelector('input')?.setAttribute('aria-describedby', element[0].getAttribute('__validationId'));
