ff082b529359cf6abd6113b4f7d5bf813d84b8a7
PriyankaPatnala <priyanka.patnala@wavemaker.com>
2023-07-24 10:45:58 +0530

    [WMS-21795] Merging branch hotfix-wme-11.2.0.5 to branch hotfix wizard: Add subtitle to each step

--- projects/components/widgets/containers/wizard/src/wizard-step/wizard-step.props.ts
+++ projects/components/widgets/containers/wizard/src/wizard-step/wizard-step.props.ts
@@ -11,7 +11,8 @@ export const registerProps = () => {
                 ['doneiconclass', {value: 'wi wi-done', ...PROP_STRING}],
                 ['name', PROP_STRING],
                 ['show', {value: true, ...PROP_BOOLEAN}],
-                ['title', {value: 'Step Title', ...PROP_STRING}]
+                ['title', {value: 'Step Title', ...PROP_STRING}],
+                ['subtitle', {value: 'Step SubTitle', ...PROP_STRING}]
             ]
         )
     );
