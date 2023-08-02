0a968f0b6523c3e96bb48d89a3ce640c01e94715
bhargavig_500297 <bhargavig_500297@WMHYLTAP030.local>
2023-08-02 11:38:41 +0530

    WMS-24049: Multiple dialogs issues fixed

--- projects/components/widgets/dialogs/default/src/base-dialog.ts
+++ projects/components/widgets/dialogs/default/src/base-dialog.ts
@@ -8,7 +8,6 @@ import {AbstractDialogService, closePopover, findRootContainer, generateGUId, is
 
 import { BaseComponent, IDialog, IWidgetConfig } from '@wm/components/base';
 import { createFocusTrap } from '@wavemaker/focus-trap/dist/focus-trap';
-
 declare const _;
 
 let eventsRegistered = false;
@@ -91,9 +90,12 @@ export abstract class BaseDialog extends BaseComponent implements IDialog, OnDes
                 focusTrapObj.activeElement = document.activeElement;
             }),
             this.bsModal.onHidden.subscribe((closeReason) => {
-                const ref = (closeReason === 'esc' 
-                    || closeReason === 'backdrop-click'
-                    || (this.dialogId && closeReason?.id === this.dialogId)) ? this.dialogService.getLastOpenedDialog() : this.dialogService.getDialogRefFromClosedDialogs();
+                let ref = this.dialogService.getDialogRefFromClosedDialogs();
+                if (this.dialogId && closeReason?.id === this.dialogId) {
+                    ref = this;
+                } else if (closeReason === 'esc' || closeReason === 'backdrop-click') {
+                    ref = this.dialogService.getLastOpenedDialog();
+                }
                 if (ref === this) {
                     // remove the dialog reference from opened dialogs and closed dialogs
                     this.dialogService.removeFromOpenedDialogs(ref);
