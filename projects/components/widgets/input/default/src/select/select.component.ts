c5b6d88540331b238620c6d84d1abb513ec5c78e
bhargavig_500297 <bhargavig_500297@WMHYLTAP030.local>
2024-01-23 11:47:15 +0530

    WMS-25005: Fix for select widget floating label on IOS
    (cherry picked from commit 3fdf6ed2ca8d0bcaa8916cf683e79c1ffe8c1064)

--- projects/components/widgets/input/default/src/select/select.component.ts
+++ projects/components/widgets/input/default/src/select/select.component.ts
@@ -1,7 +1,7 @@
 import { AfterViewInit, Component, ElementRef, Injector, ViewChild } from '@angular/core';
 import { NG_VALUE_ACCESSOR, NG_VALIDATORS } from '@angular/forms';
 
-import { DataSource, removeAttr, setAttr } from '@wm/core';
+import {DataSource, removeAttr, setAttr, App} from '@wm/core';
 import { provideAsWidgetRef, provideAs, styler } from '@wm/components/base';
 import { DatasetAwareFormComponent } from '../dataset-aware-form.component';
 
@@ -33,6 +33,7 @@ export class SelectComponent extends DatasetAwareFormComponent implements AfterV
     public name: string;
     public autofocus: boolean;
     public hint: string;
+    private app: App;
 
     @ViewChild('select', { static: true, read: ElementRef }) selectEl: ElementRef;
 
@@ -42,14 +43,19 @@ export class SelectComponent extends DatasetAwareFormComponent implements AfterV
         }
     }
 
-    constructor(inj: Injector) {
+    constructor(inj: Injector,  app: App) {
         super(inj, WIDGET_CONFIG);
+        this.app = app;
         this.acceptsArray = true;
     }
 
     ngAfterViewInit() {
         super.ngAfterViewInit();
         styler(this.selectEl.nativeElement as HTMLElement, this);
+        setTimeout(() => {
+            this.checkForFloatingLabel(null);
+        }, 10)
+
     }
 
     // Change event is registered from the template, Prevent the framework from registering one more event
@@ -96,10 +102,16 @@ export class SelectComponent extends DatasetAwareFormComponent implements AfterV
     checkForFloatingLabel($event) {
         const captionEl = $(this.selectEl.nativeElement).closest('.app-composite-widget.caption-floating');
         if (captionEl.length > 0) {
-            if ($event.type === 'focus' && (!this.datavalue || (this.datavalue && $(this.selectEl).find('select option:selected').text() === '' && this.placeholder))) {
-                $(this.selectEl.nativeElement).find('option:first').text(this.placeholder);
-            } else if (!this.datavalue) {
-                $(this.selectEl.nativeElement).find('option:selected').text('');
+            if ((!$event || $event.type === 'focus') && (!this.datavalue || (this.datavalue && $(this.selectEl).find('select option:selected').text() === '' && this.placeholder))) {
+                if(!$event){
+                    this.app.notify('captionPositionAnimate', {isSelect: true, nativeEl: captionEl});
+                }
+                if(this.placeholder) {
+                    $(this.selectEl.nativeElement).find('option:first').text(this.placeholder);
+                }
+            }
+            else if (!this.datavalue) {
+              //  $(this.selectEl.nativeElement).find('option:first').text('');
                 captionEl.removeClass('float-active');
             }
         }
