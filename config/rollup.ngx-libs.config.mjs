31fdde667949cbc7a4ff3811c3943c52006a6f95
SairamaKrishnaB <sairama.bonala@wavemaker.com>
2024-03-04 15:24:41 +0530

    WMS-25166: Angular 16 supporting external library upgrades

--- config/rollup.ngx-libs.config.mjs
+++ config/rollup.ngx-libs.config.mjs
@@ -2,7 +2,7 @@ import rollupGlobals, {rollupExternals} from './rollup-utils.mjs';
 
 export default [
     {
-        input: './node_modules/ngx-color-picker/fesm2015/ngx-color-picker.mjs',
+        input: './node_modules/ngx-color-picker/fesm2022/ngx-color-picker.mjs',
         external: [ ...rollupExternals ],
         output: {
             file: './node_modules/ngx-color-picker/bundles/ngx-color-picker.umd.js',
@@ -12,7 +12,7 @@ export default [
         }
     },
     {
-        input: './node_modules/angular-imask/fesm2015/angular-imask.mjs',
+        input: './node_modules/angular-imask/fesm2022/angular-imask.mjs',
         external: [ ...rollupExternals ],
         output: {
             file: './node_modules/angular-imask/bundles/angular-imask.umd.js',
--- config/rollup.ngx-toastr.config.mjs
+++ config/rollup.ngx-toastr.config.mjs
@@ -1,7 +1,7 @@
 import rollupGlobals, {rollupExternals} from './rollup-utils.mjs';
 
 export default {
-    input: './node_modules/ngx-toastr/fesm2015/ngx-toastr.mjs',
+    input: './node_modules/ngx-toastr/fesm2022/ngx-toastr.mjs',
     external: [ ...rollupExternals ],
     output: {
         file: './node_modules/ngx-toastr/bundles/ngx-toastr.umd.js',
