2f37c6e4cea6f5673531790a699ffddc58236bf8
SairamaKrishnaB <sairama.bonala@wavemaker.com>
2024-04-05 14:05:43 +0530

    Fix for WMS-25470:Angular build was failing due to Javascript heap out of memory after 11.6.1 release

--- build-scripts/build.js
+++ build-scripts/build.js
@@ -1,18 +1,22 @@
 const { spawnSync } = require('child_process');
 const path = require('path');
 
-const args = process.argv.slice(2);
-
+let processArgs = process.argv;
+const nodeVMArgs = processArgs.pop();
+const args = processArgs.slice(2);
 const ngBuildArgs = ['build', ...args];
 console.log("Build params - ", ngBuildArgs);
 
 //Trigger angular build with the passed params
 const ngPath = path.resolve(process.cwd(), 'node_modules', '.bin', "ng");
-const ngBuildProcess = spawnSync(ngPath, ngBuildArgs, { stdio: 'inherit', shell: true });
+const nodeArgs = [nodeVMArgs, ngPath, ...ngBuildArgs];
+console.log('Executing command: node', nodeArgs.join(' '));
+const ngBuildProcess = spawnSync('node', nodeArgs, {stdio: 'inherit', shell: true});
 
 if (ngBuildProcess.status === 0) {
     console.log('ng build completed successfully!');
 } else {
+    // TODO: JS heap out of memory error handling
     console.error('Error during ng build:', ngBuildProcess.error || ngBuildProcess.stderr);
     process.exit(1);
 }
