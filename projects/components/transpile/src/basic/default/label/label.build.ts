cb24b851f3c397658aaf517e8189e218c0f293a1
Vasundharakondareddy <vasundhara.kondareddy@wavemaker.com>
2024-05-09 14:37:02 +0530

    WMS-25499 label widget changes for ADA compliance

--- projects/components/transpile/src/basic/default/label/label.build.ts
+++ projects/components/transpile/src/basic/default/label/label.build.ts
@@ -1,12 +1,22 @@
 import { getAttrMarkup, IBuildTaskDef, register } from '@wm/transpiler';
 import {IDGenerator} from "@wm/core";
 
-const tagName = 'label';
+let tagName = 'p';
 const idGen = new IDGenerator('wm_label');
 
 register('wm-label', (): IBuildTaskDef => {
-    return {
-        pre: (attrs) => {
+    return {   
+        pre: (attrs:any) => {
+            if(!attrs.get("notag")){
+                if(!attrs.get('type')){
+            const classList=attrs.get('class') ? attrs.get('class').split(' ').filter(element => ["h1","h2","h3","h4","h5","h6","p"].includes(element)):[];
+            attrs.set('type',classList.length ? classList[0] :  "p");
+                }                
+            tagName= attrs.get('type');
+            }
+            else{
+                tagName='label';
+            }
             const counter = idGen.nextUid();
             return `<${tagName} wmLabel #${counter}="wmLabel" [attr.aria-label]="${counter}.hint" ${getAttrMarkup(attrs)}>`;
         },
