feac0acd85acac4777f5674ecc2dc0e7258d20ac
bhargavig_500297 <bhargavi.gunda@wavemaker.com>
2024-05-15 12:33:36 +0530

    babel rc

---
+++ projects/components/widgets/advanced/marquee/jest.config.js
@@ -0,0 +1,15 @@
+const baseConfig = require('../../jest.config');
+
+
+module.exports = {
+  ...baseConfig,
+  moduleNameMapper: {
+    '@core/(.*)': '<rootDir>/src/app/core/$1',
+  },
+  preset: 'jest-preset-angular',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/projects/components/widgets/advanced/marquee/tsconfig.spec.json',
+    },
+  },
+};
