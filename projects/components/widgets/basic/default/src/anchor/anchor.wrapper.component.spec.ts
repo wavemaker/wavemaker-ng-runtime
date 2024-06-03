6edc6b72d5fea09638c494464e66dbcad28d61cf
Ponnarasi <ponnarasi.a@techconative.com>
2024-06-03 09:54:18 +0530

    Added testcases for select and anchor Componenet

--- projects/components/widgets/basic/default/src/anchor/anchor.wrapper.component.spec.ts
+++ projects/components/widgets/basic/default/src/anchor/anchor.wrapper.component.spec.ts
@@ -2,8 +2,10 @@ import {Component, ViewChild} from "@angular/core";
 import {AnchorComponent} from "./anchor.component";
 import {ComponentTestBase, ITestComponentDef, ITestModuleDef} from "../../../../../base/src/test/common-widget.specs";
 import {ComponentsTestModule} from "../../../../../base/src/test/components.test.module";
+import { ComponentFixture } from "@angular/core/testing";
+import { compileTestComponent, getHtmlSelectorElement } from "projects/components/base/src/test/util/component-test-util";
 
-const markup = `<a wmAnchor data-identifier="anchor" #wm_anchor1="wmAnchor" [attr.aria-label]="wm_anchor1.hint || wm_anchor1.caption || 'Link'" hint="Link" tabindex="0" badgevalue="1" name="anchor1"></a>`;
+const markup = `<a wmAnchor data-identifier="anchor" #wm_anchor1="wmAnchor" [attr.aria-label]="wm_anchor1.hint || wm_anchor1.caption || 'Link'" hint="Link" hyperlink="http://www.google.com/doodle4google/images/splashes/featured.png" tabindex="0" badgevalue="1" name="anchor1"></a>`;
 
 @Component({
     template: markup
@@ -31,3 +33,75 @@ TestBase.verifyPropsInitialization();
 TestBase.verifyCommonProperties();
 TestBase.verifyStyles();
 TestBase.verifyAccessibility();
+
+describe("AnchorComponent", () => {
+    let wrapperComponent: AnchorWrapperComponent;
+    let wmComponent: AnchorComponent;
+    let fixture: ComponentFixture<AnchorWrapperComponent>;
+    let hyperlink = "http://www.google.com/doodle4google/images/splashes/featured.png"
+    beforeEach(() => {
+        fixture = compileTestComponent(testModuleDef, AnchorWrapperComponent);
+        wrapperComponent = fixture.componentInstance;
+        wmComponent = wrapperComponent.wmComponent;
+        fixture.detectChanges();
+    });
+
+    it("should create anchor component", () => {
+        expect(wmComponent).toBeTruthy();
+    });
+
+    it("should have badge value", () => {
+        expect(wmComponent.badgevalue).toBe("1");
+    });
+
+    it("should have tabindex", () => {
+        let tabindex =  getHtmlSelectorElement(fixture, 'a').nativeElement.attributes.getNamedItem('tabindex');
+        expect(parseInt(tabindex.value)).toBe(0);
+    });
+    
+    it("should have identifier", () => {
+        let identifier =  getHtmlSelectorElement(fixture, 'a').nativeElement.attributes.getNamedItem('data-identifier');
+        expect(identifier.value).toBe("anchor");
+    });
+
+    it("should have hyperlink", () => {
+        expect(wmComponent.hyperlink).toBe(hyperlink);
+    });
+    
+    it("should trigger onPropertyChange", () => {
+        jest.spyOn(wmComponent, 'onPropertyChange');
+        wmComponent.onPropertyChange("hyperlink", hyperlink, hyperlink);
+        expect(wmComponent.hyperlink).toBe(hyperlink);
+    });
+
+    it("should trigger onPropertyChange without nv value", () => {
+        jest.spyOn(wmComponent, 'onPropertyChange');
+        wmComponent.onPropertyChange("hyperlink", "", hyperlink);
+        expect(wmComponent.hyperlink).toBe(hyperlink);
+    });
+
+    it("should trigger onPropertyChange with encodeurl", () => {
+        wmComponent.encodeurl = true;
+        jest.spyOn(wmComponent, 'onPropertyChange');
+        wmComponent.onPropertyChange("hyperlink", hyperlink, hyperlink);
+        expect(wmComponent.hyperlink).toBe(hyperlink);
+    });
+
+    it("should trigger onPropertyChange with www.", () => {
+        jest.spyOn(wmComponent, 'onPropertyChange');
+        wmComponent.onPropertyChange("hyperlink", "www.google.com", hyperlink);
+        expect(wmComponent.hyperlink).toBe(`http://www.google.com/doodle4google/images/splashes/featured.png`);
+    });
+
+    it("should trigger ngOnAttach", () => {
+        jest.spyOn(wmComponent, 'ngOnAttach');
+        wmComponent.ngOnAttach();
+        expect(wmComponent.nativeElement.getAttribute('aria-label')).toBe("Link");
+    });
+
+    it("should onActive callback", () => {
+        jest.spyOn(wmComponent, 'onActive');
+        wmComponent.onActive(() => {});
+    });
+   
+});
