79598a0dbd9e0d6b997b85681c63b6a94cb62b93
sivaramakrishnan-techconative <sivaramakrishnan.srivatsan@techconative.com>
2024-06-21 10:12:23 +0530

    [WMS-25708] Added unit test cases for navbar comp

---
+++ projects/components/widgets/navigation/navbar/src/navbar.wrapper.component.spec.ts
@@ -0,0 +1,59 @@
+import { Component, ViewChild } from '@angular/core';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar.component'; // Make sure to replace these with actual paths
+import { App } from '@wm/core';
+import { ITestModuleDef, ITestComponentDef, ComponentTestBase } from 'projects/components/base/src/test/common-widget.specs';
+
+let mockApp = {
+    subscribe: () => { return () => { } }
+};
+
+const markup = `<div wmNavbar name="navbar1" aria-label="navbar" hint="navbar" tabindex="1"  ></div>`;
+
+@Component({
+    template: markup
+})
+class NavbarWrapperComponent {
+    @ViewChild(NavbarComponent, { static: true }) wmComponent: NavbarComponent;
+}
+
+const testModuleDef: ITestModuleDef = {
+    imports: [FormsModule],
+    declarations: [NavbarWrapperComponent, NavbarComponent],
+    providers: [
+        { provide: App, useValue: mockApp },
+    ]
+};
+
+const componentDef: ITestComponentDef = {
+    $unCompiled: $(markup),
+    type: 'wm-navbar',
+    widgetSelector: '[wmNavbar]',
+    testModuleDef: testModuleDef,
+    testComponent: NavbarWrapperComponent,
+    inputElementSelector: 'input'
+};
+
+const TestBase: ComponentTestBase = new ComponentTestBase(componentDef);
+TestBase.verifyPropsInitialization();
+TestBase.verifyCommonProperties();
+TestBase.verifyStyles();
+TestBase.verifyAccessibility();
+
+describe('Navbar component', () => {
+    let wrapperComponent: NavbarWrapperComponent;
+    let navbarComponent: NavbarComponent;
+    let fixture: ComponentFixture<NavbarWrapperComponent>;
+
+    beforeEach(waitForAsync(() => {
+        fixture = TestBed.configureTestingModule(testModuleDef).createComponent(NavbarWrapperComponent);
+        wrapperComponent = fixture.componentInstance;
+        navbarComponent = wrapperComponent.wmComponent;
+        fixture.detectChanges();
+    }));
+
+    it('should create navbar component', () => {
+        expect(wrapperComponent).toBeTruthy();
+    });
+});
