d2a926eb86d27e55af8beac683d8d7aac87a220a
Ashok Kumar Rajendran <ashok.rajendran@wavemaker.com>
2024-07-08 17:56:45 +0530

    Added change listeners to handle changes on exposed properties

--- projects/components/base/src/widgets/common/base/custom-widget-container.directive.ts
+++ projects/components/base/src/widgets/common/base/custom-widget-container.directive.ts
@@ -6,46 +6,9 @@ import { WidgetRef } from "../../framework/types";
 declare const _;
 
 @Directive({
-    selector: '[customWidgetContainer]'
+    selector: '[customWidgetContainer][widgetname]'
 })
 export class CustomWidgetContainerDirective {
-    private contentInitialized = false;
-    private $target;
-
-    async _renderCustom(widgetName) {
-        // destroy the existing partial
-        this.vcRef.clear();
-
-        // when the container-target is inside the component template, it can be queried after viewInit of the component.
-        $invokeWatchers(true);
-
-        const componentFactory = await this.customWidgetRefProvider.getComponentFactoryRef(widgetName, ComponentType.WIDGET);
-
-        if (componentFactory) {
-            const instanceRef = this.vcRef.createComponent(componentFactory, 0, this.inj);
-            // if (instanceRef && instanceRef['instance'] && prefabName) {
-            //     // @ts-ignore
-            //     instanceRef['instance'].prefabName = prefabName;
-            // }
-            if (!this.$target) {
-                // this.$target = this.elRef.nativeElement.querySelector('[partial-container-target]') || this.elRef.nativeElement;
-                this.$target = this.elRef.nativeElement;
-            }
-
-            this.$target.innerHTML = '';
-            this.$target.appendChild(instanceRef.location.nativeElement);
-            this.contentInitialized = true;
-            setTimeout(() => this.onLoadSuccess(), 200);
-        }
-    }
-
-    renderCustom = _.debounce(this._renderCustom, 200);
-
-    onLoadSuccess() {
-        this.componentInstance.invokeEventCallback('load');
-
-        this.app.notify('partialLoaded');
-    }
 
     constructor(
         @Self() @Inject(WidgetRef) public componentInstance,
@@ -54,19 +17,13 @@ export class CustomWidgetContainerDirective {
         public inj: Injector,
         private app: App,
         @Attribute('widgetname') widgetname: string,
-        private resolver: ComponentFactoryResolver,
         private customWidgetRefProvider: CustomWidgetRefProvider
     ) {
-        componentInstance.registerPropertyChangeListener((key: string, nv: any, ov?: any) => {
-            if (key === 'widgetname') {
-                if (componentInstance.$lazyLoad) {
-                    componentInstance.$lazyLoad = () => {
-                        this.renderCustom(nv);
-                        componentInstance.$lazyLoad = noop;
-                    };
-                } else {
-                    this.renderCustom(nv);
-                }
+        const widgetnames = this.componentInstance.widgetname;
+        this.customWidgetRefProvider.getComponentFactoryRef(widgetname, ComponentType.WIDGET).then((componentFactory) => {
+            if (componentFactory) {
+                const instanceRef = this.vcRef.createComponent(componentFactory, 0, this.inj);
+                this.elRef.nativeElement.appendChild(instanceRef.location.nativeElement);
             }
         });
     }
--- projects/components/widgets/advanced/custom/src/custom-widget-container/custom-widget-container.directive.ts
+++ projects/components/widgets/advanced/custom/src/custom-widget-container/custom-widget-container.directive.ts
@@ -1,15 +1,19 @@
-import {Attribute, Directive, Injector, OnInit, Optional, SecurityContext} from '@angular/core';
+import { Attribute, Directive, ElementRef, Injector, OnInit, Optional, SecurityContext } from '@angular/core';
 
-import {setCSS, setProperty} from '@wm/core';
-import { IWidgetConfig, provideAsWidgetRef, SanitizePipe, StylableComponent, styler } from '@wm/components/base';
+import { noop } from '@wm/core';
+import { IWidgetConfig, PROP_TYPE, provideAsWidgetRef, register, SanitizePipe, StylableComponent, styler } from '@wm/components/base';
 
-import { registerProps } from './custom-widget.props';
+import { customWidgetProps } from './custom-widget.props';
+
+const registeredPropsSet = new Set<string>();
 
 const DEFAULT_CLS = 'app-html-container';
 const WIDGET_CONFIG: IWidgetConfig = {
     widgetType: 'wm-custom-widget',
     hostClass: DEFAULT_CLS
 };
+
+declare const _;
 
 @Directive({
     selector: '[wmWidgetContainer]',
@@ -19,38 +23,61 @@ const WIDGET_CONFIG: IWidgetConfig = {
     exportAs: 'wmWidgetContainer'
 })
 export class CustomWidgetContainerDirective extends StylableComponent implements OnInit {
-    static initializeProps = registerProps();
-    public content: string;
-    public prop: any = {};
+    widgetType: string;
+    name: string;
+    propsReady: Function;
+    widgetName: string;
 
     constructor(
-        inj: Injector,
-        @Attribute('height') height: string,
+        inj: Injector, elRef: ElementRef,
+        @Attribute('widgetname') widgetname: string,
     ) {
-        super(inj, WIDGET_CONFIG);
-
-        // if the height is provided set the overflow to auto
-        if (height) {
-            setCSS(this.nativeElement, 'overflow', 'auto');
-        }
+        let resolveFn: Function = noop;
+        super(inj, WIDGET_CONFIG, new Promise(res => resolveFn = res));
+        this.propsReady = resolveFn;
+        this.widgetType = WIDGET_CONFIG.widgetType;
+        this.widgetName = widgetname;
+        this.name = elRef.nativeElement.getAttribute('name');
 
         styler(this.nativeElement, this);
+
+        // Call on property change on name to set name attribute on element.
+        this.registerReadyStateListener(() => {
+            super.onPropertyChange('name', this.name);
+        });
     }
 
-    ngOnInit() {
-        super.ngOnInit();
-        const attrs = this.nativeElement.attributes;
-        for (let attrName, attrVal, i = 0; i < attrs.length; i++){
-            attrName = attrs[i].nodeName;
-            attrVal = attrs[i].nodeValue;
-            if (attrName.startsWith('prop-')) {
-                attrName = attrName.replace('prop-', '');
-                this.prop[attrName] = attrVal;
-            }
+    public setProps(config) {
+        if (!config || !config.properties) {
+            return;
         }
+        if (!registeredPropsSet.has(this.widgetType)) {
+            register(this.widgetType, this.prepareProps(config.properties));
+        }
+
+        this.propsReady();
     }
 
-    onPropertyChange(key: string, nv: any, ov?: any) {
-        super.onPropertyChange(key, nv, ov);
+    private prepareProps(props = {}) {
+        const propsMap = new Map(customWidgetProps);
+        // propsMap.set('widgetname', this.widgetName);
+        Object.entries(props).forEach(([k, v]: [string, any]) => {
+            let type = PROP_TYPE.STRING;
+
+            if (v.type === 'boolean') {
+                type = PROP_TYPE.BOOLEAN;
+            } else if (v.type === 'number') {
+                type = PROP_TYPE.NUMBER;
+            } else if (v.type !== 'string') {
+                type = v.type;
+            }
+
+            // Do not set the 'bind:*' values
+            propsMap.set(k, { type, value: _.startsWith(v.value, 'bind:') ? undefined : v.value });
+        });
+
+        registeredPropsSet.add(this.widgetType);
+
+        return propsMap;
     }
 }
