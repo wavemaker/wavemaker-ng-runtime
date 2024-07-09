7430260c784aed0fb92bd204d0a4a068fd8a9d1c
sivaramakrishnan-techconative <sivaramakrishnan.srivatsan@techconative.com>
2024-07-09 14:20:48 +0530

    WMS-25708 : Written test cases for checkbox

--- projects/components/widgets/input/default/src/checkbox/checkbox.wrapper.component.spec.ts
+++ projects/components/widgets/input/default/src/checkbox/checkbox.wrapper.component.spec.ts
@@ -1,27 +1,26 @@
-import {Component, ViewChild} from "@angular/core";
-import {CheckboxComponent} from "./checkbox.component";
-import {waitForAsync, ComponentFixture} from "@angular/core/testing";
-import {compileTestComponent, getHtmlSelectorElement, mockApp} from "../../../../../base/src/test/util/component-test-util";
-import {ComponentTestBase, ITestComponentDef, ITestModuleDef} from "../../../../../base/src/test/common-widget.specs";
-import {FormsModule} from "@angular/forms";
-import {App} from "@wm/core";
+import { Component, ViewChild } from "@angular/core";
+import { CheckboxComponent } from "./checkbox.component";
+import { waitForAsync, ComponentFixture } from "@angular/core/testing";
+import { compileTestComponent, getHtmlSelectorElement, mockApp } from "../../../../../base/src/test/util/component-test-util";
+import { ComponentTestBase, ITestComponentDef, ITestModuleDef } from "../../../../../base/src/test/common-widget.specs";
+import { FormsModule } from "@angular/forms";
+import { App } from "@wm/core";
 
 const markup = `<div wmCheckbox hint="checkbox" caption="Label" name="checkbox1" tabindex="1"></div>`;
-
 
 @Component({
     template: markup
 })
 
 class CheckboxWrapperComponent {
-    @ViewChild(CheckboxComponent, /* TODO: add static flag */ {static: true}) wmComponent: CheckboxComponent
+    @ViewChild(CheckboxComponent, /* TODO: add static flag */ { static: true }) wmComponent: CheckboxComponent
 }
 
 const testModuleDef: ITestModuleDef = {
     imports: [FormsModule],
     declarations: [CheckboxWrapperComponent, CheckboxComponent],
     providers: [
-        {provide: App, useValue: mockApp},
+        { provide: App, useValue: mockApp },
     ]
 };
 
@@ -62,9 +61,59 @@ describe('Checkbox component', () => {
     it('should check checkbox  on keyboard enter', () => {
         expect(checkboxComponent.datavalue).toBeFalsy();
         const checkboxElement = getHtmlSelectorElement(fixture, '[wmCheckbox]');
-        checkboxElement.triggerEventHandler('keydown.enter', { preventDefault: () => {}});
+        checkboxElement.triggerEventHandler('keydown.enter', { preventDefault: () => { } });
         fixture.whenStable().then(() => {
             expect(checkboxComponent.datavalue).toBeTruthy();
+        });
+    });
+    describe('onPropertyChange method', () => {
+        beforeEach(() => {
+            checkboxComponent['_onChange'] = jest.fn();
+            jest.spyOn(Object.getPrototypeOf(CheckboxComponent.prototype), 'onPropertyChange').mockImplementation(() => { });
+        });
+
+        afterEach(() => {
+            jest.restoreAllMocks();
+        });
+
+        it('should not call _onChange when key is tabindex', () => {
+            checkboxComponent.onPropertyChange('tabindex', '2', '1');
+            expect(checkboxComponent['_onChange']).not.toHaveBeenCalled();
+        });
+
+        it('should call _onChange when key is required', () => {
+            checkboxComponent.onPropertyChange('required', true, false);
+            expect(checkboxComponent['_onChange']).toHaveBeenCalledWith(checkboxComponent.datavalue);
+        });
+
+        it('should update _caption when key is caption', () => {
+            checkboxComponent.onPropertyChange('caption', 'New Caption', 'Old Caption');
+            expect(checkboxComponent['_caption']).toBe('New Caption');
+
+            checkboxComponent.onPropertyChange('caption', '', 'Old Caption');
+            expect(checkboxComponent['_caption']).toBe('&nbsp;');
+        });
+
+        it('should update _checkedvalue when key is checkedvalue', () => {
+            checkboxComponent.onPropertyChange('checkedvalue', 'true', 'false');
+            expect(checkboxComponent['_checkedvalue']).toBe(true);
+        });
+
+        it('should update _uncheckedvalue when key is uncheckedvalue', () => {
+            checkboxComponent.onPropertyChange('uncheckedvalue', 'false', 'true');
+            expect(checkboxComponent['_uncheckedvalue']).toBe(false);
+        });
+
+        it('should update datavalue and call _onChange when key is datavalue', () => {
+            checkboxComponent.onPropertyChange('datavalue', 'true', 'false');
+            expect(checkboxComponent.datavalue).toBe(true);
+            expect(checkboxComponent['_onChange']).toHaveBeenCalledWith(true);
+        });
+
+        it('should call super.onPropertyChange for other keys', () => {
+            checkboxComponent.onPropertyChange('someOtherKey', 'newValue', 'oldValue');
+            expect(Object.getPrototypeOf(CheckboxComponent.prototype).onPropertyChange)
+                .toHaveBeenCalledWith('someOtherKey', 'newValue', 'oldValue');
         });
     });
 });
