67005988682493255bb646121e5d78db8e74822b
nareshr <naresh.ravulapalli@wavemaker.com>
2024-07-16 07:33:48 +0530

    WMS-24855 - Sonatype Nexus Repo. Changes made to the production publish flow to add a new repo for angular app which has a dependency on ui-variables and ng-runtime. This dependency issue is fixed. These changes are required as at wm app build time, we copy angular-app package.json and run npm install which is breaking the nexus flow design. To fix this we are creating a new package with the name angular-app and all its dependencies and then this is used to install the required deps

---
+++ bundle-angular-app-cli.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+BASEDIR=$(dirname "$0")
+cd $BASEDIR
+dev=true
+publish=false
+publishVersion='x.x.x'
+while [ "$1" != "" ]; do
+    case $1 in
+        --publish-version)
+            shift
+            publishVersion=$1
+            dev=false
+            publish=true
+        ;;
+    esac
+    shift
+done
+
+cp -r src dist/runtime-cli/angular-app
+cp -r build-scripts dist/runtime-cli/angular-app
+cp -r pwa-assets dist/runtime-cli
+if [[ "${dev}" == true ]]; then
+    cp -r libraries dist/runtime-cli/angular-app
+fi
+cp -rf angular.json package.json package-lock.json .npmrc tsconfig.json tsconfig.web-app.json wm-custom-webpack.config.js dist/runtime-cli/angular-app
+cp ./wm.package.json libraries/package.json
+
+if [[ "${publish}" == true ]]; then
+    node bundle-angular-app-cli.js --publishVersion=${publishVersion}
+fi
+
+mkdir -p dist/npm-packages/angular-app
+cp -rf dist/runtime-cli/angular-app/ dist/npm-packages/angular-app
+
+cd dist/npm-packages/angular-app
+npm install
+npm pack
+cp "wavemaker-angular-app-${publishVersion}.tgz" "../"
+
