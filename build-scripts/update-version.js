6cdcc3782f696c1398521f9156f57363da0318e1
varsha.gaddam <varsha.gaddam@wavemaker.com>
2024-07-26 12:33:23 +0530

    WMS-26012 :Corrected dependency name in update-version.js

--- build-scripts/update-version.js
+++ build-scripts/update-version.js
@@ -4,14 +4,14 @@ const yargs = require('yargs');
 const argv = yargs(process.argv).argv;
 
 /**
- * Updates the @wavemaker/ng-runtime dependency in the specified package.json file.
+ * Updates the @wavemaker/app-ng-runtime dependency in the specified package.json file.
  * @param {string} path - The path to the package.json file to update.
  */
 
 const updateNgRuntimePackage = (path) => {
     if (fs.existsSync(path)) {
         const packageJSON = require('../' + path);
-        packageJSON['dependencies']['@wavemaker/ng-runtime'] = argv["publish-version"];
+        packageJSON['dependencies']['@wavemaker/app-ng-runtime'] = argv["publish-version"];
         fs.writeFileSync(path, JSON.stringify(packageJSON, null, 4));
     } else {
         console.log('package.json not found at ' + path);
