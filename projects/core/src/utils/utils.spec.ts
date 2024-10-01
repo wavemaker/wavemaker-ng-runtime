732e526794a70feffdb97fd4e83ab7bd6887917e
Ponnarasi <ponnarasi.a@techconative.com>
2024-10-01 11:46:42 +0530

    WMS-26598 : Revert x2js unit testcase

--- projects/core/src/utils/utils.spec.ts
+++ projects/core/src/utils/utils.spec.ts
@@ -1472,7 +1472,7 @@ describe('xmlToJson', () => {
         const result = xmlToJson(xmlString);
         expect(result).toEqual(expectedJson);
         expect((global as any).X2JS).toHaveBeenCalledWith({
-            'emptyNodeForm': 'object',
+            'emptyNodeForm': 'content',
             'attributePrefix': '',
             'enableToStringFunc': false
         });
