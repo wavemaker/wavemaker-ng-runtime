f5281d17777ab8a4d37aee4803e027b36e686a23
SairamaKrishnaB <sairama.bonala@wavemaker.com>
2024-10-15 14:45:24 +0530

    WMS-26705: Uglification of js is not happening in 10.x angular deployment

--- wm-custom-webpack.config.js
+++ wm-custom-webpack.config.js
@@ -1,35 +1,52 @@
-const CompressionPlugin = require(`compression-webpack-plugin`);
-const BrotliPlugin = require(`brotli-webpack-plugin`);
-const path = require(`path`);
+/**
+ * There is an issue with Angular 11 build where compression is happening before uglification, resulting in these unuglified files.
+ * This is a temporary fix to move compression after uglification, not required in latest WMO 11.8.5(Angular 17).
+ **/
+const path = require('path');
+const fs = require('fs');
+
+class PostBuildCompressionPlugin {
+    apply(compiler) {
+        compiler.hooks.done.tap("PostBuildCompressionPlugin", (stats) => {
+            const outputPath = stats.compilation.outputOptions.path;
+            setTimeout(() => {
+                const assets = stats.toJson().assets;
+                assets.forEach((asset) => {
+                    const filePath = path.join(outputPath, asset.name);
+                    if (fs.existsSync(filePath) && /.(js|css|html)$/.test(asset.name)) {
+                        const content = fs.readFileSync(filePath, "utf8");
+                        const gzip = require("zlib").gzipSync;
+                        const compressed = gzip(content);
+                        const path = require('path');
+                        // Parse the filename to create the new gzip filename
+                        const parsedPath = path.parse(asset.name);
+                        const gzipFileName = parsedPath.name + '.gzip' + parsedPath.ext;
+                        const gzipFilePath = path.join(outputPath, gzipFileName);
+                        fs.writeFileSync(gzipFilePath, compressed);
+                    }
+                });
+            }, 1000);
+        });
+    }
+}
+
 module.exports = {
-    resolve:{
-        alias:{
-            themes: path.resolve(__dirname,`src/assets/themes/`)
+    resolve: {
+        alias: {
+            themes: path.resolve(__dirname, `src/assets/themes/`)
         }
     },
-    plugins:[
-        new BrotliPlugin({
-            asset: '[fileWithoutExt].br.[ext]',
-            test: /\.(js|css|html|svg|txt|eot|otf|ttf|gif)$/
-        }),
-        new CompressionPlugin({
-            test: /\.(js|css|html|svg|txt|eot|otf|ttf|gif)$/,
-            filename(info){
-                let opFile= info.path.split('.'),
-                opFileType =  opFile.pop(),
-                opFileName = opFile.join('.');
-                return `${opFileName}.gzip.${opFileType}`;
-            }
-        })
+    plugins: [
+        new PostBuildCompressionPlugin()
     ],
     optimization: {
         splitChunks: {
-            automaticNameDelimiter:'-',
+            automaticNameDelimiter: '-',
             cacheGroups: {
                 vendor: {
                     minSize: 1000000,
                     maxSize: 1000000,
-                    test: /[\\/]node_modules(?![\\/]wm)[\\/]/
+                    test: /[/]node_modules(?![/]wm)[/]/
                 }
             }
         }
