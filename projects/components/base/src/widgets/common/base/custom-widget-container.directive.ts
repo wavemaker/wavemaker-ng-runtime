e8ade4e06c1b41c4465f29315b4c3ccb601d5f30
vaishnavi.chandrasekharan <vaishnavi.chandrasekharan@wavemaker.com>
2024-11-21 17:21:44 +0530

    [WMS-26255] Removed attr passing logic in CW directive

--- projects/components/base/src/widgets/common/base/custom-widget-container.directive.ts
+++ projects/components/base/src/widgets/common/base/custom-widget-container.directive.ts
@@ -21,15 +21,7 @@ export class CustomWidgetContainerDirective {
         private customWidgetRefProvider: CustomWidgetRefProvider
     ) {
         const widgetnames = this.componentInstance.widgetname;
-        const cwMarkupEl = this.componentInstance.$element;
-        const cwMarkupAttr = cwMarkupEl.prop("attributes");
-        const cwMarkupAttrArr = $.map(cwMarkupAttr, (attribute) => {
-            if (attribute.name.startsWith('prop-') || this.customWidgetBasePropList.indexOf(attribute.name) > -1) {
-                return;
-            }
-            return attribute.name + '=' + `"${attribute.value}"`;
-        });
-        this.customWidgetRefProvider.getComponentFactoryRef(widgetname, ComponentType.WIDGET, { baseWidgetAttr: cwMarkupAttrArr }).then((componentFactory) => {
+        this.customWidgetRefProvider.getComponentFactoryRef(widgetname, ComponentType.WIDGET).then((componentFactory) => {
             if (componentFactory) {
                 const instanceRef = this.vcRef.createComponent(componentFactory, 0, this.inj);
                 this.elRef.nativeElement.appendChild(instanceRef.location.nativeElement);
