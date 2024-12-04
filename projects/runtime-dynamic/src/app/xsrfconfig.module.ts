7f1598c3511ad9c539b76e1d9433264fe166253b
Ponnarasi <ponnarasi.a@techconative.com>
2024-12-04 22:41:08 +0530

    WMS-26916 : fixed unit testcase

--- projects/runtime-dynamic/src/app/xsrfconfig.module.ts
+++ projects/runtime-dynamic/src/app/xsrfconfig.module.ts
@@ -2,33 +2,27 @@ import { APP_INITIALIZER, NgModule } from "@angular/core";
 import { HttpClientXsrfModule } from "@angular/common/http";
 import { SecurityService } from "@wm/security";
 
-export function initializeXsrfConfig(
-    securityService: SecurityService
-) {
-    return async () => {
-        try {
-            await securityService.load();
-            
-            const xsrfCookieName = 
-                (window as any)._WM_APP_PROPERTIES["securityInfo"]?.csrfCookieName 
-                || "wm_xsrf_token";
-            
-            const xsrfHeaderName = 
-                (window as any)._WM_APP_PROPERTIES["securityInfo"]?.csrfHeaderName 
-                || null;
+export function initializeXsrfConfig(securityService: SecurityService) {
+    return () => {
+            securityService.load().then(() => {
+                const xsrfCookieName =
+                    (window as any)._WM_APP_PROPERTIES["securityInfo"]
+                        ?.csrfCookieName || "wm_xsrf_token";
 
-            const xsrfOptions: any = {
-                cookieName: xsrfCookieName
-            };
+                const xsrfHeaderName =
+                    (window as any)._WM_APP_PROPERTIES["securityInfo"]
+                        ?.csrfHeaderName || null;
 
-            if (xsrfHeaderName) {
-                xsrfOptions.headerName = xsrfHeaderName;
-            }
+                const xsrfOptions: any = {
+                    cookieName: xsrfCookieName,
+                };
 
-            HttpClientXsrfModule.withOptions(xsrfOptions);
-        } catch (error) {
-            console.error("Error initializing XSRF configuration", error);
-        }
+                if (xsrfHeaderName) {
+                    xsrfOptions.headerName = xsrfHeaderName;
+                }
+
+                HttpClientXsrfModule.withOptions(xsrfOptions);
+            });
     };
 }
 
@@ -37,9 +31,8 @@ export function initializeXsrfConfig(
     providers: [
         {
             provide: APP_INITIALIZER,
-            useFactory: (
-                securityService: SecurityService
-            ) => initializeXsrfConfig(securityService),
+            useFactory: (securityService: SecurityService) =>
+                initializeXsrfConfig(securityService),
             deps: [SecurityService],
             multi: true,
         },
