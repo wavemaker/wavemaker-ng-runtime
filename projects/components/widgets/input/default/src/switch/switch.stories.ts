b03279eacfc86243822a77c0500c58bd5818f575
Venkateswarlu Kakani <venkateswarlu.kakani@wavemaker.com>
2024-12-05 14:00:41 +0530

    story added for switch

--- projects/components/widgets/input/default/src/switch/switch.stories.ts
+++ projects/components/widgets/input/default/src/switch/switch.stories.ts
@@ -11,8 +11,38 @@ const meta: Meta<SwitchComponent> = {
 export default meta;
 type Story = StoryObj<SwitchComponent>;
 
-export const Primary: Story = {
+export const Default: Story = {
   args: {
     disabled: false,
+    dataset : ['yes', 'no', 'maybe'],
   },
 };
+
+export const Required: Story = {
+  args: {
+    required: true,
+    dataset: ['yes', 'no', 'maybe'],
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    disabled: true,
+    dataset: ['yes', 'no', 'maybe'],
+  },
+};
+export const DisplayfieldChange: Story = {
+  args: {
+    disabled: false,
+    dataset: [
+      { "name": "John Doe", "location": "New York, USA" },
+      { "name": "Jane Smith", "location": "London, UK" },
+      { "name": "Michael Johnson", "location": "Sydney, Australia" },
+      { "name": "Emily Davis", "location": "Toronto, Canada" },
+      { "name": "David Williams", "location": "Berlin, Germany" }
+    ],
+    displayfield : "location"
+  },
+};
+
+
