2763e6aedbc94768b704b1c98f845c7c5d23c295
PriyankaBhadri24 <priyanka.bhadri@wavemaker.com>
2024-12-05 17:07:32 +0530

    updated number.stories.ts

--- projects/components/widgets/input/default/src/number/number.stories.ts
+++ projects/components/widgets/input/default/src/number/number.stories.ts
@@ -5,17 +5,56 @@ const meta: Meta<NumberComponent> = {
   title: 'Form/Number',
   component: NumberComponent,
   tags: ['autodocs'],
-  argTypes: {}
+  argTypes: {
+    inputmode: { control: 'select', options: ['natural', 'financial'] },
+  }
 };
 
 export default meta;
 type Story = StoryObj<NumberComponent>;
 
-export const Primary: Story = {
+export const Placeholder: Story = {
   args: {
-    minvalue: 0,
-    maxvalue: 100,
-    datavalue: 10,
-    readonly: false
+    placeholder: 'EnterNumber here',
+      minvalue: 0,
+      maxvalue: 100,
+      datavalue: 10,
+      
+    },
+
+};
+
+export const Disabled: Story = {
+  args: {
+  
+    disabled: true,
+    placeholder:"This is disabled"
   },
 };
+
+export const Readonly: Story = {
+  args: {
+    
+    readonly: true,
+    hint: 'This is only readable', 
+  },
+};
+
+export const validation: Story = {
+  args: {
+   required:true,
+    maxvalue: 10,
+    minvalue: 2,
+    datavalue :5
+
+  },
+};
+
+export const InputMode: Story = {
+  args: {
+   inputmode:"natural",
+   decimalplaces:2,
+   trailingzero:false
+  },
+};
+
