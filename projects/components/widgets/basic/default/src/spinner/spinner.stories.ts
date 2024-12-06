54734bb39ff065202d70e2d31b531272dea563ac
PriyankaBhadri24 <priyanka.bhadri@wavemaker.com>
2024-12-06 15:30:02 +0530

    updated variants for spinner

--- projects/components/widgets/basic/default/src/spinner/spinner.stories.ts
+++ projects/components/widgets/basic/default/src/spinner/spinner.stories.ts
@@ -5,12 +5,38 @@ const meta: Meta<SpinnerComponent> = {
   title: 'Form/Spinner',
   component: SpinnerComponent,
   tags: ['autodocs'],
-  argTypes: {}
+ 
+    argTypes: {
+      type: { control: 'select', options: ['icon', 'image'] },
+    }
+  
 };
 
 export default meta;
 type Story = StoryObj<SpinnerComponent>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    iconclass:'wm-sl-r sl-loading',
+    animation:'flash',
+    showCaption:true
+
+  },
 };
+
+export const IconSpinner: Story = {
+  args: {
+   type:'icon',
+  iconclass:'wm-sl-r sl-loading',
+  animation:'flash',
+  showCaption:true
+  },
+};
+
+export const ImageSpinner : Story = {
+  args:{
+    // image:'',
+    imageheight:'100px',
+    imagewidth:'100px',
+  }
+}
