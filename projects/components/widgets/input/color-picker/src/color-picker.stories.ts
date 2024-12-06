7148f95d70d667bcc0022630358f4d5a37b4db08
Venkateswarlu Kakani <venkateswarlu.kakani@wavemaker.com>
2024-12-06 13:50:22 +0530

    Story added for color picker

--- projects/components/widgets/input/color-picker/src/color-picker.stories.ts
+++ projects/components/widgets/input/color-picker/src/color-picker.stories.ts
@@ -7,7 +7,13 @@ const meta: Meta<ColorPickerComponent> = {
   title: 'Form/ColorPicker',
   component: ColorPickerComponent,
   tags: ['autodocs'],
-  argTypes: {}
+  argTypes: {
+    autoclose: {
+    control: 'select',
+    options: ['outsideClick', 'always', 'disabled'],
+    defaultValue: undefined,
+  },
+}
 };
 
 export default meta;
@@ -15,5 +21,22 @@ type Story = StoryObj<ColorPickerComponent>;
 
 export const Default: Story = {
   args: {
+    name: 'ColorPicker',
+    placeholder: 'Select Color',
+    readonly: false,
+    required: false,
+  },
+  // Exclude `autoclose` from the Default story
+  parameters: {
+    controls: {
+      exclude: ['autoclose'],
+    },
+  },
+};
+
+export const Autoclose: Story = {
+  args: {
+    placeholder: 'Select Color',
+    autoclose:'outsideClick',
   },
 };
