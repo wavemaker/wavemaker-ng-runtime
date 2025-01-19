768078058209a7f0eac7e99498e965d891c94cb4
rajkumarabbadi <rajkumar.abbadi@wavemaker.com>
2025-01-19 20:24:29 +0530

    Changes - Fixes

--- .storybook/manager.js
+++ .storybook/manager.js
@@ -1,6 +1,9 @@
 import { addons } from '@storybook/manager-api';
 import { themes } from '@storybook/theming';
 import waveTheme from './wavedesign-theme';
+import "./theme.css";
+
+
 
 
 addons.setConfig({
--- .storybook/wavedesign-theme.js
+++ .storybook/wavedesign-theme.js
@@ -6,4 +6,34 @@ export default create({
   brandUrl: 'https://wavemaker.com',
   brandImage: 'https://wm-storybook.s3.us-east-1.amazonaws.com/Logo.png',
   brandTarget: '_self',
+
+  // fontBase: "neurial-grotesk-regular",
+
+  // Colors
+  // colorPrimary: "rgba(135, 135, 135, 1)",
+  colorSecondary: "#296df6",
+
+  // UI
+  appBg:
+    "linear-gradient(170deg, rgb(226, 244, 254) 14.38%, rgb(218, 217, 252) 99.86%)",
+  appContentBg: "#ffffff",
+  appPreviewBg: "#ffffff",
+  // appBorderColor: "rgba(0, 0, 0, 0.10)",
+  appBorderRadius: 6,
+
+  // Text colors
+  textColor: "rgba(48, 48, 48, 1)",
+  textInverseColor: "#ffffff",
+
+  // Toolbar default and active colors
+  // barTextColor: "#5E686F",
+  // barSelectedColor: "rgba(41, 109, 246, 0.14)",
+  // barHoverColor: "#5E686F",
+  // barBg: "#fff",
+
+  // Form colors
+  // inputBg: "#ffffff",
+  // inputBorder: "rgba(0, 0, 0, 0.10)",
+  // inputTextColor: "rgba(48, 48, 48, 1)",
+  // inputBorderRadius: 4,
 });
