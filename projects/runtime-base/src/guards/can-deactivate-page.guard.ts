636aadb68f760fa70b0a4e7eb447ff30d246b2bf
sivaramakrishnan-techconative <sivaramakrishnan.srivatsan@techconative.com>
2025-01-27 14:25:08 +0530

    WMS-27416 : Fixed an issue where the target page is coming as undefined in onBeforePageChange event.

--- projects/runtime-base/src/guards/can-deactivate-page.guard.ts
+++ projects/runtime-base/src/guards/can-deactivate-page.guard.ts
@@ -1,11 +1,10 @@
-import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
-import {noop} from '@wm/core';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { noop } from '@wm/core';
 import { ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 
-
-export interface CanComponentDeactivate  {
-    canDeactivate: () => Observable<boolean> | Promise<boolean> | boolean;
+export interface CanComponentDeactivate {
+    canDeactivate: (nextState?: string) => Observable<boolean> | Promise<boolean> | boolean;
 }
 
 /*
@@ -16,33 +15,36 @@ export interface CanComponentDeactivate  {
  */
 @Injectable()
 export class CanDeactivatePageGuard {
-  canDeactivate(
-    component: CanComponentDeactivate,
-    currentRoute: ActivatedRouteSnapshot,
-    currentState: RouterStateSnapshot,
-    nextState?: RouterStateSnapshot
-  ) {
-      function invokeCompDeactivate() {
-          let retVal;
-          const nextRoute = nextState ? nextState.url : undefined;
-          // Calling onBeforePageLeave method present at page level
-          retVal = this.app.activePage && this.app.activePage.onBeforePageLeave(nextRoute);
-          // Calling onBeforePageLeave method present at app level only if page level method return true
-          // or if there is no page level method
-          if (retVal !== false ) {
-              retVal =  (this.app.onBeforePageLeave || noop)(this.app.activePageName, this.app.activePage, nextRoute);
-          }
-          return retVal === undefined ? true : retVal;
-      }
-      return invokeCompDeactivate.apply(component);
-  }
+    canDeactivate(
+        component: CanComponentDeactivate,
+        currentRoute: ActivatedRouteSnapshot,
+        currentState: RouterStateSnapshot,
+        nextState?: RouterStateSnapshot
+    ) {
+        function invokeCompDeactivate() {
+            let retVal;
+            const nextRoute = nextState ? nextState.url : undefined;
+            // Calling onBeforePageLeave method present at page level
+            retVal = this.app.activePage && this.app.activePage.onBeforePageLeave(nextRoute);
+            // Calling onBeforePageLeave method present at app level only if page level method return true
+            // or if there is no page level method
+            if (retVal !== false) {
+                retVal = (this.app.onBeforePageLeave || noop)(this.app.activePageName, this.app.activePage, nextRoute);
+            }
+            return retVal === undefined ? true : retVal;
+        }
+        return invokeCompDeactivate.apply(component);
+    }
 }
-
 
 //  [WMS-18847] - canDeactivate guard for angular deployment.
 @Injectable()
 export class CanDeactivateNgPageGuard {
-    canDeactivate(component: CanComponentDeactivate) {
-        return component.canDeactivate ? component.canDeactivate() : true;
+    canDeactivate(component: CanComponentDeactivate,
+        currentRoute?: ActivatedRouteSnapshot,
+        currentState?: RouterStateSnapshot,
+        nextState?: RouterStateSnapshot) {
+        const nextRoute = nextState ? nextState.url : undefined;
+        return component.canDeactivate ? component.canDeactivate(nextRoute) : true;
     }
 }
