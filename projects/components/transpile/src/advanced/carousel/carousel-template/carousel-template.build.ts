b6fbcf439d91c944340c0d5d6430c4591e361a1a
mahesh.reddy <mahesh.reddy@wavemaker.com>
2025-02-10 17:54:42 +0530

    WMS-27492: Runtime codebase: updating old control flows to new control flows manually

--- projects/components/transpile/src/advanced/carousel/carousel-template/carousel-template.build.ts
+++ projects/components/transpile/src/advanced/carousel/carousel-template/carousel-template.build.ts
@@ -16,12 +16,15 @@ register('wm-carousel-template', (): IBuildTaskDef => {
         requires: ['wm-carousel'],
         pre: (attrs, shared, parentCarousel) => {
             const carouselRef = parentCarousel.get('carousel_ref');
-            return `<div *ngIf="!${carouselRef}.fieldDefs">{{${carouselRef}.nodatamessage}}</div>
-                    <${carouselContentTagName} wmCarouselTemplate #carouselTemplateRef="carouselTemplateRef" ${getAttrMarkup(attrs)} *ngFor="let item of ${carouselRef}.fieldDefs; let i = index;">
+            return `@if(!${carouselRef}.fieldDefs){<div >{{${carouselRef}.nodatamessage}}</div>}
+                    @for (item of ${carouselRef}.fieldDefs; track item; let i = $index) {
+                        console.log(i);
+                        <${carouselContentTagName} wmCarouselTemplate #carouselTemplateRef="carouselTemplateRef" ${getAttrMarkup(attrs)}>
                         <ng-container [ngTemplateOutlet]="carouselTempRef${counter}"
-                            [ngTemplateOutletContext]="{item:item, index:i}"
-                            [ngTemplateOutletInjector]="${carouselRef}.createCustomInjector('carousel_item_' + carouselTemplateRef.trackId, {item:item, index:i})"></ng-container>
-                    </${carouselContentTagName}>
+                        [ngTemplateOutletContext]="{item:item, index:i}"
+                        [ngTemplateOutletInjector]="${carouselRef}.createCustomInjector('carousel_item_' + carouselTemplateRef.trackId, {item:item, index:i})"></ng-container>
+                        </${carouselContentTagName}>
+                       }
                     <ng-template #carouselTempRef${counter++} let-item="item" let-index="index">`;
         },
         post: () => `</ng-template>`
--- projects/components/transpile/src/containers/repeat-template.build.ts
+++ projects/components/transpile/src/containers/repeat-template.build.ts
@@ -11,7 +11,7 @@ register('wm-repeat-template', (): IBuildTaskDef => {
         pre: (attrs, shared, parentAccordion, parentTab) => {
             const widgetRef = (parentAccordion && parentAccordion.get('accordion_ref')) || (parentTab && parentTab.get('tabs_ref'));
             if (widgetRef) {
-                return `<div *ngIf="${widgetRef}.fieldDefs && !${widgetRef}.fieldDefs.length">{{${widgetRef}.nodatamessage}}</div>
+                return `@if(${widgetRef}.fieldDefs && !${widgetRef}.fieldDefs.length){<div>{{${widgetRef}.nodatamessage}}</div>}
                     <${dynamicTemplateTagName} wmRepeatTemplate #repeatItemRef="repeatItemRef" ${getAttrMarkup(attrs)} *ngFor="let item of ${widgetRef}.fieldDefs; let i = index;">
                         <ng-container [ngTemplateOutlet]="widgetRef${counter}"
                             [ngTemplateOutletContext]="{item:item, index:i}"
